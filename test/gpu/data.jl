7ed5961391e1176bdad0d7d355ae7155513f0051
Simon Byrne <simonbyrne@gmail.com>
2021-04-12 10:45:19 -0700

    add test set

--- test/gpu/data.jl
+++ test/gpu/data.jl
@@ -15,14 +15,17 @@ function knl_copy!(dst, src)
     return nothing
 end
 
-S = Tuple{Complex{Float64}, Float64}
-src = IJFH{S}(CuArray(rand(4, 4, 3, 10)))
-dst = IJFH{S}(CuArray(zeros(4, 4, 3, 10)))
-
 function test_copy!(dst, src)
     @cuda threads = (4, 4) blocks = (10,) knl_copy!(dst, src)
 end
 
-test_copy!(dst, src)
+@testset "data in GPU kernels" begin
 
-@test getfield(dst, :array) == getfield(src, :array)
+    S = Tuple{Complex{Float64}, Float64}
+    src = IJFH{S}(CuArray(rand(4, 4, 3, 10)))
+    dst = IJFH{S}(CuArray(zeros(4, 4, 3, 10)))
+
+    test_copy!(dst, src)
+
+    @test getfield(dst, :array) == getfield(src, :array)
+end
