fa337a7fbc17a3cdbdb53fd0b58b7808a04d62e7
Jake Bolewski <jakebolewski@gmail.com>
2021-07-07 16:57:54 -0600

    reformat

--- examples/column/adv_diff.jl
+++ examples/column/adv_diff.jl
@@ -26,7 +26,7 @@ z₀ = FT(0)
 z₁ = FT(10)
 t₀ = FT(0)
 t₁ = FT(10)
-μ = FT(-1/2)
+μ = FT(-1 / 2)
 ν = FT(5)
 𝓌 = FT(1)
 δ = FT(1)
@@ -37,26 +37,18 @@ mesh = Meshes.IntervalMesh(domain, nelems = n)
 cs = Spaces.CenterFiniteDifferenceSpace(mesh)
 fs = Spaces.FaceFiniteDifferenceSpace(cs)
 zc = Fields.coordinate_field(cs)
-zp = z₀+z₁/n/2:z₁/n:z₁-z₁/n/2
+zp = (z₀ + z₁ / n / 2):(z₁ / n):(z₁ - z₁ / n / 2)
 
 
-function gaussian(z,t;
-                  μ = -1//2,
-                  ν = 1,
-                  𝓌 = 1,
-                  δ = 1)
-    return exp(-(z-μ-𝓌*t)^2/(4*ν*(t+δ)))/sqrt(1+t/δ)
+function gaussian(z, t; μ = -1 // 2, ν = 1, 𝓌 = 1, δ = 1)
+    return exp(-(z - μ - 𝓌 * t)^2 / (4 * ν * (t + δ))) / sqrt(1 + t / δ)
 end
-function ∇gaussian(z,t;
-                   μ = -1//2,
-                   ν = 1,
-                   𝓌 = 1,
-                   δ = 1)
+function ∇gaussian(z, t; μ = -1 // 2, ν = 1, 𝓌 = 1, δ = 1)
     return -2 * (z - μ - 𝓌 * t) / (4 * ν * (δ + t)) *
-    exp(-(z - μ - 𝓌 * t)^2 / (4 * ν * (δ + t))) / sqrt(1 + t / δ)
+           exp(-(z - μ - 𝓌 * t)^2 / (4 * ν * (δ + t))) / sqrt(1 + t / δ)
 end
 
-T = gaussian.(zc,-0; μ=μ,δ=δ,ν=ν,𝓌=𝓌)
+T = gaussian.(zc, -0; μ = μ, δ = δ, ν = ν, 𝓌 = 𝓌)
 V = ones(FT, fs)
 
 # Solve Adv-Diff Equation: ∂_t T = α ∇²T
@@ -66,27 +58,25 @@ z₁ = FT(10)
 function ∑tendencies!(dT, T, z, t)
 
     ic2f = Operators.InterpolateC2F()
-    bc_vb = Operators.SetValue(FT(gaussian(z₀,t;ν = ν, δ=δ, 𝓌 = 𝓌, μ = μ)))
-    bc_vt = Operators.SetValue(FT(gaussian(z₁,t;ν = ν, δ=δ, 𝓌 = 𝓌, μ = μ)))
-    bc_gb = Operators.SetGradient(FT(∇gaussian(z₀,t; ν = ν, δ=δ, 𝓌 = 𝓌, μ = μ)))
-    bc_gt = Operators.SetGradient(FT(∇gaussian(z₁,t; ν = ν, δ=δ, 𝓌 = 𝓌, μ = μ)))
- 
-#   Upwind Biased Product
-#   UB = Operators.UpwindBiasedProductC2F(
-#       bottom = Operators.Extrapolate(),
-#       top = bc_vt,
-#   )
-#   ∂ = Operators.GradientF2C()
-#   return @. dT = -∂(UB(V, ic2f(T)))
+    bc_vb = Operators.SetValue(FT(gaussian(z₀, t; ν = ν, δ = δ, 𝓌 = 𝓌, μ = μ)))
+    bc_vt = Operators.SetValue(FT(gaussian(z₁, t; ν = ν, δ = δ, 𝓌 = 𝓌, μ = μ)))
+    bc_gb =
+        Operators.SetGradient(FT(∇gaussian(z₀, t; ν = ν, δ = δ, 𝓌 = 𝓌, μ = μ)))
+    bc_gt =
+        Operators.SetGradient(FT(∇gaussian(z₁, t; ν = ν, δ = δ, 𝓌 = 𝓌, μ = μ)))
 
-    A = Operators.AdvectionC2C(
-        bottom = bc_vb,
-        top = Operators.Extrapolate(),
-    )
+    #   Upwind Biased Product
+    #   UB = Operators.UpwindBiasedProductC2F(
+    #       bottom = Operators.Extrapolate(),
+    #       top = bc_vt,
+    #   )
+    #   ∂ = Operators.GradientF2C()
+    #   return @. dT = -∂(UB(V, ic2f(T)))
+
+    A = Operators.AdvectionC2C(bottom = bc_vb, top = Operators.Extrapolate())
 
 
-    gradc2f = Operators.GradientC2F(bottom = bc_vb,
-                                    top = bc_gt)
+    gradc2f = Operators.GradientC2F(bottom = bc_vb, top = bc_gt)
     gradf2c = Operators.GradientF2C()
 
     return @. dT = gradf2c(ν * gradc2f(T)) - A(V, T)
@@ -115,19 +105,38 @@ dirname = "adv_diff"
 path = joinpath(@__DIR__, "output", dirname)
 mkpath(path)
 
-anim = Plots.@animate for (nt,u) in enumerate(sol.u)
-    Plots.plot(u, xlim = (0, 1), ylim = (-1,10), title = "$(nt-1) s", lc=:black, 
-               lw=2, 
-               ls=:dash, 
-               label = "Approx Sol.", 
-               legend=:outerright, 
-               m=:o, 
-               xlabel="T(z)", 
-               ylabel="z")
-    Plots.plot!(gaussian.(zp,nt-1;μ = μ, δ=δ, ν=ν, 𝓌=𝓌),zp, xlim = (0, 1), ylim = (-1,10), title = "$(nt) s", lc=:red, lw=2, label="Analytical Sol.", m=:x)
+anim = Plots.@animate for (nt, u) in enumerate(sol.u)
+    Plots.plot(
+        u,
+        xlim = (0, 1),
+        ylim = (-1, 10),
+        title = "$(nt-1) s",
+        lc = :black,
+        lw = 2,
+        ls = :dash,
+        label = "Approx Sol.",
+        legend = :outerright,
+        m = :o,
+        xlabel = "T(z)",
+        ylabel = "z",
+    )
+    Plots.plot!(
+        gaussian.(zp, nt - 1; μ = μ, δ = δ, ν = ν, 𝓌 = 𝓌),
+        zp,
+        xlim = (0, 1),
+        ylim = (-1, 10),
+        title = "$(nt) s",
+        lc = :red,
+        lw = 2,
+        label = "Analytical Sol.",
+        m = :x,
+    )
 end
 Plots.mp4(anim, joinpath(path, "adv_diff.mp4"), fps = 10)
-Plots.png(Plots.plot(sol.u[end], xlim = (0, 1)), joinpath(path, "adv_diff_end.png"))
+Plots.png(
+    Plots.plot(sol.u[end], xlim = (0, 1)),
+    joinpath(path, "adv_diff_end.png"),
+)
 
 function linkfig(figpath, alt = "")
     # buildkite-agent upload figpath
