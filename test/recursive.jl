cf42eb6805d2a93a4c156f7b500e7ae4afe5043a
Simon Byrne <simonbyrne@gmail.com>
2021-08-02 13:32:37 -0700

    add JETTest, test on 1.7

---
+++ test/recursive.jl
@@ -0,0 +1,16 @@
+using Test, JETTest
+
+using ClimaCore.RecursiveApply
+
+for x in [
+    1.0,
+    1.0f0,
+    (1.0, 2.0),
+    (1.0f0, 2.0f0),
+    (a = 1.0, b = (x1 = 2.0, x2 = 3.0)),
+    (a = 1.0f0, b = (x1 = 2.0f0, x2 = 3.0f0)),
+]
+    @test_nodispatch 2 ⊠ x
+    @test_nodispatch x ⊞ x
+    @test_nodispatch RecursiveApply.rdiv(x, 3)
+end
