b3444dd9d60c4b6727fd4f7438739869134db20d
Katherine Deck <kdeck@caltech.edu>
2021-08-03 16:41:44 -0700

    fixed things based on convo this morning, added in bc types

--- examples/column/richards_vg.jl
+++ examples/column/richards_vg.jl
@@ -18,6 +18,149 @@ using Printf
 
 const FT = Float64
 
+struct SoilParams{FT <: AbstractFloat}
+    ν::FT
+    vgn::FT
+    vgα::FT
+    vgm::FT
+    ksat::FT
+    θr::FT
+    S_s::FT
+end
+
+abstract type bc end
+
+struct θDirichlet{FT <: AbstractFloat} <: bc
+    θvalue::FT
+end
+struct FreeDrainage <: bc end
+struct RunoffBC{FT <: AbstractFloat} <: bc
+    rain_rate::FT
+end
+struct NormalFlux{FT <: AbstractFloat} <: bc
+    #bc actually on minus the flux
+    negative_flux_val::FT
+end
+
+function effective_saturation(θ; ν = ν, θr = θr)
+    
+    if θ < θr
+        println("Effective saturation is negative")
+        println(θ)
+    end
+    return (θ-θr)/(ν-θr)
+end
+
+function pressure_head(S; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
+    if S < FT(0)
+        println("Effective saturation is negative")
+        println(S)
+    end
+    if S < FT(1)
+            ψ = -((S^(-FT(1) / vgm) - FT(1)) * vgα^(-vgn))^(FT(1) / vgn)
+    else
+        θ = S* (ν-θr) + θr 
+        ψ = (θ-ν)/S_s
+    end
+    
+    return ψ
+end
+
+
+function hydraulic_conductivity(S; vgm = vgm, ksat = ksat)
+    if S < FT(1)
+        K = sqrt(S) * (FT(1) - (FT(1) - S^(FT(1) / vgm))^vgm)^FT(2)
+    else
+        K = FT(1)
+    end
+        return K*ksat
+end
+function hydrostatic_profile(z, zmin, porosity, n, α, θr)
+    myf = eltype(z)
+    m = FT(1 - 1 / n)
+    S = FT((FT(1) + (α * (z - zmin))^n)^(-m))
+    return FT(S * (porosity-θr)+θr)
+end
+
+
+function compute_richards_rhs!(dθl, θl, p, top::θDirichlet,bottom::FreeDrainage)
+    # θ_top = something, K∇h_bottom = K_bottom (∇h = 1). If ∇h = 1, θ_b = θ_f, so K = Kbottom at the face.
+
+    sp = p[2]
+    zc = p[1]
+    @unpack ν,vgn,vgα,vgm,ksat,θr = sp
+    S = effective_saturation.(θl; ν = ν, θr = θr)
+    K = hydraulic_conductivity.(S; vgm = vgm, ksat = ksat)
+    ψ = matric_potential.(S; vgn = vgn, vgα = vgα, vgm = vgm)
+    h = ψ .+ zc
+    
+    
+    θ_top = θl_surf
+    S_top = effective_saturation.(θ_top; ν = ν, θr = θr)
+    h_top = matric_potential(S_top; vgn = vgn, vgα = vgα, vgm = vgm)#ztop = 0
+    K_top = hydraulic_conductivity(S_top; vgm = vgm, ksat = ksat)
+    bc_ktop = Operators.SetValue(K_top)
+    If = Operators.InterpolateC2F(;
+                                  top = bc_ktop)
+    
+    bc_t = Operators.SetValue(h_top)
+    gradc2f = Operators.GradientC2F(top = bc_t) # set value on h_top
+    
+    bc_b = Operators.SetValue(FT(1)*parent(K)[1])
+    gradf2c = Operators.GradientF2C(bottom = bc_b) # set value on K∇h at bottom
+    
+    
+    
+    return @. dθl = gradf2c( If(K) * gradc2f(h))
+end
+
+
+function compute_richards_rhs!(dθl, θl, p, top::RunoffBC, bottom::NormalFlux)
+    # Dirichet at top or flux BC at top, depending on rain and ic.
+    # No flux at bottom
+    sp = p[2]
+    zc = p[1]
+    @unpack ν,vgn,vgα,vgm,ksat,θr = sp
+
+    S = effective_saturation.(θl; ν = ν, θr = θr)
+    K = hydraulic_conductivity.(S; vgm = vgm, ksat = ksat)
+    ψ = pressure_head.(S; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
+    h = ψ .+ zc
+    # compute ic
+    θ_top  = ν
+    S_top = effective_saturation.(θ_top; ν = ν, θr = θr)
+    ψ_top = pressure_head(S_top; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
+    h_top = ψ_top #(z_top = 0)
+    bc_t = Operators.SetValue(h_top) # gradient is determined from this and h_interior
+    bc_b = Operators.SetGradient(FT(0)) # This can be anything b.c we just care about top value.
+    
+    
+    # ponding BC
+    gradc2f = Operators.GradientC2F(bottom = bc_b, top = bc_t)
+    grad_h_ponding = gradc2f(h)
+    K_top = ksat
+    ic = parent(grad_h_ponding)[end]*K_top
+    
+    
+    if rain_rate < ic # ponding:
+        gradc2f = Operators.GradientC2F(top = bc_t)
+        If = Operators.InterpolateC2F(top = Operators.SetValue(K_top))
+        flux_bc_b = Operators.SetValue(FT(0))
+        gradf2c = Operators.GradientF2C(bottom = flux_bc_b) # no flux at bottom
+        return @. dθl = gradf2c(If(K)*gradc2f(h))
+    else
+        #flux_top = rain_rate
+        If_rain = Operators.InterpolateC2F()
+        top_flux = -rain_rate
+        bottom_flux = FT(0)
+        gradc2f_rain = Operators.GradientC2F()
+        gradf2c = Operators.GradientF2C(top = Operators.SetValue(top_flux), bottom = Operators.SetValue(bottom_flux))
+        return @. dθl = gradf2c(If_rain(K) * gradc2f_rain(h))
+        
+    end
+
+end
+
 
 
 @testset "Richards sand 1" begin
@@ -35,97 +178,33 @@ const FT = Float64
     Δt = FT(0.5)
     tf = FT(60 * 60 * 0.8)
     t0 = FT(0)
-    
-    
+
+    msp = SoilParams{FT}(ν,vgn,vgα,vgm, ksat, θr, FT(1e-4))
+    bottom_bc = FreeDrainage()
+    top_bc = θDirichlet(θl_surf)
     domain = Domains.IntervalDomain(z₀, z₁, x3boundary = (:bottom, :top))
     mesh = Meshes.IntervalMesh(domain, nelems = n)
     
     cs = Spaces.CenterFiniteDifferenceSpace(mesh)
     fs = Spaces.FaceFiniteDifferenceSpace(cs)
     zc = Fields.coordinate_field(cs)
-    #zp = (z₀ + z₁ / n / 2):(z₁ / n):(z₁ - z₁ / n / 2)
-    
-    ###doesnt work without kwargs?
-    ###ERROR: cannot infer concrete eltype of effective_saturation on (Float64,)
-    
-    function effective_saturation(θ; ν = ν, θr = θr)
-        
-        if θ < θr
-            println("Effective saturation is negative")
-            println(θ)
-        end
-        
-        if θ > ν
-            println("Effective saturation is positive")
-            println(θ)
-        end
-        return (θ-θr)/(ν-θr)
-    end
-    
-    function matric_potential(S; vgn = vgn, vgα = vgα, vgm = vgm)
-        if S < FT(0)
-            println("Effective saturation is negative")
-            println(S)
-        end
-        
-        if S > FT(1)
-            println("Effective saturation is positive")
-            println(S)
-        end
-        
-        ψ_m = -((S^(-FT(1) / vgm) - FT(1)) * vgα^(-vgn))^(FT(1) / vgn)
-        return ψ_m
-    end
-    
-    function hydraulic_conductivity(S; vgm = vgm, ksat = ksat)
-        if S < FT(1)
-            K = sqrt(S) * (FT(1) - (FT(1) - S^(FT(1) / vgm))^vgm)^FT(2)
-        else
-            K = FT(1)
-        end
-        return K*ksat
-    end
     
     
     θl = Fields.zeros(FT, cs) .+ θl_0
     
     # Solve Richard's Equation: ∂_t θl = ∂_z [K(θl)(∂_z ψ(θl) +1)]
     
-    
-    function ∑tendencies!(dθl, θl, z, t)
-        # h_top = something, ∇h_bottom = something (grad and state values set on same thing)
-        θ_top = θl_surf
-        S_top = effective_saturation.(θ_top; ν = ν, θr = θr)
-        h_top = matric_potential(S_top; vgn = vgn, vgα = vgα, vgm = vgm)#ztop = 0
-        bc_t = Operators.SetValue(h_top) # gradient is determined from this and h_interior
-        bc_b = Operators.SetGradient(FT(1)) # forces ∇h to be this by setting h_face appropriately
-        
-        gradc2f = Operators.GradientC2F(bottom = bc_b, top = bc_t)
-        gradf2c = Operators.GradientF2C()
-                
-    
-        S = effective_saturation.(θl; ν = ν, θr = θr)
-        K = hydraulic_conductivity.(S; vgm = vgm, ksat = ksat)
-        ψ = matric_potential.(S; vgn = vgn, vgα = vgα, vgm = vgm)
-        h = ψ .+ zc
-        #In this case, we have a dirichlet on h at the top -> known top value of
-        # θ. so we should have a known value of K at the top to agree with.
-        K_top = hydraulic_conductivity(S_top; vgm = vgm, ksat = ksat)
-        bc_ktop = Operators.SetValue(K_top)
-        # At the bottom, we have a BC on grad(h). I thought this meant that h_face at the bottom was forced to be something
-        # so we should know theta_bottom_face -> K_bottom_face should be known.
-        # probably shouldnt be an extrapolation
-        If = Operators.InterpolateC2F(;
-                                      bottom = Operators.Extrapolate(),
-                                      top = bc_ktop)
-        return @. dθl = gradf2c( If(K).* gradc2f(h))
-        
+    p = [zc, msp,top_bc, bottom_bc]
+    function ∑tendencies!(dθl, θl, p, t)
+        top = p[3]
+        bot = p[4]
+        compute_richards_rhs!(dθl, θl,p , top,bot)
     end
 #@show ∑tendencies!(similar(θl), θl, nothing, 0.0);
     
     # Solve the ODE operator
     
-    prob = ODEProblem(∑tendencies!, θl, (t0, tf))
+    prob = ODEProblem(∑tendencies!, θl, (t0, tf),p)
     sol = solve(
         prob,
         Tsit5(),
@@ -183,7 +262,7 @@ end
     Δt = FT(0.1)
     tf = FT(60*60)
     t0 = FT(0)
-    
+    msp = SoilParams{FT}(ν,vgn,vgα,vgm, ksat, θr, FT(S_s))
     
     domain = Domains.IntervalDomain(z₀, z₁, x3boundary = (:bottom, :top))
     mesh = Meshes.IntervalMesh(domain, nelems = n)
@@ -192,110 +271,23 @@ end
     fs = Spaces.FaceFiniteDifferenceSpace(cs)
     zc = Fields.coordinate_field(cs)
     
-    function effective_saturation(θ; ν = ν, θr = θr)
-        
-        if θ < θr
-            println("Effective saturation is negative")
-            println(θ)
-        end
-        return (θ-θr)/(ν-θr)
-    end
-    
-    function pressure_head(S; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
-        if S < FT(0)
-            println("Effective saturation is negative")
-            println(S)
-        end
-        if S < FT(1)
-            ψ = -((S^(-FT(1) / vgm) - FT(1)) * vgα^(-vgn))^(FT(1) / vgn)
-        else
-            θ = S* (ν-θr) + θr 
-            ψ = (θ-ν)/S_s
-        end
-        
-        return ψ
-    end
-
-
-    function hydraulic_conductivity(S; vgm = vgm, ksat = ksat)
-        if S < FT(1)
-            K = sqrt(S) * (FT(1) - (FT(1) - S^(FT(1) / vgm))^vgm)^FT(2)
-        else
-            K = FT(1)
-        end
-        return K*ksat
-    end
-    function hydrostatic_profile(z, zmin, porosity, n, α, θr)
-        myf = eltype(z)
-        m = FT(1 - 1 / n)
-        S = FT((FT(1) + (α * (z - zmin))^n)^(-m))
-        return FT(S * (porosity-θr)+θr)
-    end
     θl = hydrostatic_profile.(zc, FT(-0.35), ν, vgn, vgα, θr) 
     
-    # Solve Richard's Equatio2n: ∂_t θl = ∂_z [K(θl)(∂_z ψ(θl) +1)]
     rain_rate = FT(-5e-4)
-    function ∑tendencies!(dθl, θl, z, t)
-        # compute ic
-        θ_top  = ν
-        S_top = effective_saturation.(θ_top; ν = ν, θr = θr)
-        ψ_top = pressure_head(S_top; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
-        h_top = ψ_top #(z_top = 0)
-        bc_t = Operators.SetValue(h_top) # gradient is determined from this and h_interior
-        bc_b = Operators.SetGradient(FT(0)) # This is the same as flux_bottom = 0
-        
 
-        # ponding BC
-        gradc2f = Operators.GradientC2F(bottom = bc_b, top = bc_t)
-        
-        S = effective_saturation.(θl; ν = ν, θr = θr)
-        K = hydraulic_conductivity.(S; vgm = vgm, ksat = ksat)
-        ψ = pressure_head.(S; vgn = vgn, vgα = vgα, vgm = vgm, ν = ν, θr = θr, S_s = S_s)
-        h = ψ .+ zc
-
-
-        grad_h_ponding = gradc2f(h)
-        K_top = ksat
-        bc_ktop = Operators.SetValue(K_top)
-        If = Operators.InterpolateC2F(;
-                                      bottom = Operators.Extrapolate(),
-                                      top = bc_ktop)
-        top_flux_ponding = -FT(1) .* If(K) .* grad_h_ponding
-
-        
-
-        i_c = parent(top_flux_ponding)[end]
-        if rain_rate < i_c # ponding:
-            gradf2c = Operators.GradientF2C()
-            return @. dθl = -gradf2c(top_flux_ponding)
-        else
-            #flux_top = rain_rate
-            If_rain = Operators.InterpolateC2F(;
-                                               bottom = Operators.Extrapolate(),
-                                               top = Operators.Extrapolate())
-            Kf = If_rain(K)
-            ∇h_bottom = FT(0)
-            ∇h_top = rain_rate ./ (-Kf)
-            gradc2f_rain = Operators.GradientC2F(top = Operators.SetValue(∇h_top), bottom = Operators.SetValue(∇h_bottom))
-            ∇hf = gradc2f_rain(h)
-            return @. dθl = -gradf2c(-Kf * ∇hf)
-
-        end
+    top_bc = RunoffBC{FT}(rain_rate)
+    bottom_bc = NormalFlux{FT}(FT(0.0))
+    p = [zc, msp, top_bc, bottom_bc]
+    function ∑tendencies!(dθl, θl, p, t)
+        top = p[3]
+        bot = p[4]
+        compute_richards_rhs!(dθl, θl,p , top,bot)
     end
-#@show ∑tendencies!(similar(θl), θl, nothing, 0.0);
-    
-    # Solve the ODE operator
-    # saved_values = SavedValues(Float64, typeof(θl))
-#    cb = SavingCallback((u,t,integrator)->(θl), saved_values, saveat=t0:500*Δt:tf)
-    prob = ODEProblem(∑tendencies!, θl, (t0, tf))
+    prob = ODEProblem(∑tendencies!, θl, (t0, tf),p)
     sol = solve(
         prob,
         CarpenterKennedy2N54(),
         dt = Δt,
-        #saveat = 500 * Δt,
-#        callback = cb also doesnt work
-        #progress = true,
-        #progress_message = (dt, u, p, t) -> t,
     );
 
 
