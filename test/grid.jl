1dfce6102f85a6c6bdfb3347e5247b8f169f7c1d
Valeria Barra <valeriabarra21@gmail.com>
2021-09-01 19:12:36 -0700

    Add irregular ts mesh test

--- test/grid.jl
+++ test/grid.jl
@@ -27,7 +27,7 @@ function rectangular_grid(
     return (domain, mesh, grid_topology)
 end
 
-function tensorproduct_grid(
+function regular_tensorproduct_grid(
     n1,
     n2,
     x1periodic,
@@ -46,15 +46,67 @@ function tensorproduct_grid(
         x2boundary = x2periodic ? nothing : (:south, :north),
     )
     mesh = Meshes.TensorProductMesh(domain, n1, n2)
-    ts_topology = Topologies.TensorProductTopology(mesh)
-    return (domain, mesh, ts_topology)
+    r_ts_topology = Topologies.TensorProductTopology(mesh)
+    return (domain, mesh, r_ts_topology)
+end
+
+function irregular_tensorproduct_grid(
+    n1,
+    n2,
+    x1periodic,
+    x2periodic;
+    x1min = 0.0,
+    x1max = 1.0,
+    x2min = 0.0,
+    x2max = 1.0,
+)
+    domain = Domains.RectangleDomain(
+        x1min..x1max,
+        x2min..x2max,
+        x1periodic = x1periodic,
+        x2periodic = x2periodic,
+        x1boundary = x1periodic ? nothing : (:west, :east),
+        x2boundary = x2periodic ? nothing : (:south, :north),
+    )
+
+    mesh = Meshes.EquispacedRectangleMesh(domain, n1, n2)
+    nv2 = x2periodic ? n2 : n2 + 1
+    range1 = mesh.range1
+    range2 = mesh.range2
+    elemheight = range2[2] - range2[1]
+
+    coordinates =
+        Vector{Cartesian2DPoint{typeof(x1min)}}(undef, (n1 + 1) * (n2 + 1))
+
+    # Iterate mesh vertices
+    grid_topo = Topologies.GridTopology(mesh)
+    v_iter = Topologies.VertexIterator(grid_topo)
+
+    for v in v_iter
+        (i, j) = v.num
+        # Shift 0-based indices
+        i += 1
+        j += 1
+        vcoords = Cartesian2DPoint(range1[i], range2[j])
+        # If I am on a boundary, don't apply any warping
+        if j == 1 || j == nv2
+            coordinates[(i - 1) * (n2 + 1) + j] = vcoords
+        else # Interior vertex case, shift x2-coord of a fixed amount
+            coordinates[(i - 1) * (n2 + 1) + j] =
+                Cartesian2DPoint(vcoords.x1, vcoords.x2 + 0.5 * elemheight)
+        end
+    end
+    ts_mesh = Meshes.TensorProductMesh(domain, n1, n2, coordinates)
+    i_ts_topology = Topologies.TensorProductTopology(ts_mesh)
+    return (domain, ts_mesh, i_ts_topology)
 end
 
 @testset "opposing face tests" begin
 
     _, _, grid_topology = rectangular_grid(1, 1, true, true)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, true)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "assert correct element numbering" begin
             @test_throws AssertionError Topologies.opposing_face(topology, 0, 1)
             @test_throws AssertionError Topologies.opposing_face(topology, 2, 1)
@@ -74,8 +126,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, true, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with 1 periodic boundary" begin
             _, _, topology = rectangular_grid(1, 1, true, false)
             @test Topologies.opposing_face(topology, 1, 1) == (1, 2, false)
@@ -86,8 +139,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, false, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with non-periodic boundaries" begin
             @test Topologies.opposing_face(topology, 1, 1) == (0, 1, false)
             @test Topologies.opposing_face(topology, 1, 2) == (0, 2, false)
@@ -97,8 +151,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 2, false, false)
-    _, _, ts_topology = tensorproduct_grid(2, 2, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 2, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "2×2 element quad mesh with non-periodic boundaries" begin
             @test Topologies.opposing_face(topology, 1, 1) == (0, 1, false)
             @test Topologies.opposing_face(topology, 1, 2) == (2, 1, false)
@@ -115,8 +170,9 @@ end
 @testset "simple rectangular mesh interior faces iterator" begin
 
     _, _, grid_topology = rectangular_grid(1, 1, true, true)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, true)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with all periodic boundries" begin
             @test length(Topologies.interior_faces(topology)) == 2
             faces = collect(Topologies.interior_faces(topology))
@@ -126,8 +182,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, true, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with 1 periodic boundary" begin
             @test length(Topologies.interior_faces(topology)) == 1
             faces = collect(Topologies.interior_faces(topology))
@@ -136,8 +193,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, false, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.interior_faces(topology)) == 0
             faces = collect(Topologies.interior_faces(topology))
@@ -146,8 +204,10 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 2, false, false)
-    _, _, ts_topology = tensorproduct_grid(2, 2, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 2, false, false)
+    _, _, i_ts_topology = irregular_tensorproduct_grid(2, 2, false, false)
+    topologies = (grid_topology, r_ts_topology, i_ts_topology)
+    for topology in topologies
         @testset "2×2 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.interior_faces(topology)) == 4
             faces = collect(Topologies.interior_faces(topology))
@@ -161,8 +221,9 @@ end
 
 @testset "simple rectangular mesh boundry faces iterator" begin
     _, _, grid_topology = rectangular_grid(1, 1, true, true)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, true)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with all periodic boundries" begin
             @test length(Topologies.boundary_faces(topology, 1)) == 0
             @test length(Topologies.boundary_faces(topology, 2)) == 0
@@ -176,8 +237,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, true, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with 1 periodic boundary" begin
             @test length(Topologies.boundary_faces(topology, 1)) == 0
             @test length(Topologies.boundary_faces(topology, 2)) == 0
@@ -191,8 +253,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, false, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.boundary_faces(topology, 1)) == 1
             @test length(Topologies.boundary_faces(topology, 2)) == 1
@@ -210,8 +273,10 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 3, false, false)
-    _, _, ts_topology = tensorproduct_grid(2, 3, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 3, false, false)
+    _, _, i_ts_topology = irregular_tensorproduct_grid(2, 3, false, false)
+    topologies = (grid_topology, r_ts_topology, i_ts_topology)
+    for topology in topologies
         @testset "2×3 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.boundary_faces(topology, 1)) == 3
             @test length(Topologies.boundary_faces(topology, 2)) == 3
@@ -233,8 +298,9 @@ end
 @testset "simple rectangular mesh vertex iterator" begin
 
     _, _, grid_topology = rectangular_grid(1, 1, true, true)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, true)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with all periodic boundries" begin
             @test length(Topologies.vertices(topology)) == 1
             V = collect(Topologies.vertices(topology))
@@ -245,8 +311,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, true, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with 1 periodic boundary" begin
             @test length(Topologies.vertices(topology)) == 2
             V = collect(Topologies.vertices(topology))
@@ -259,8 +326,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(1, 1, false, false)
-    _, _, ts_topology = tensorproduct_grid(1, 1, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.vertices(topology)) == 4
             V = collect(Topologies.vertices(topology))
@@ -277,8 +345,10 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 3, false, false)
-    _, _, ts_topology = tensorproduct_grid(2, 3, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 3, false, false)
+    _, _, i_ts_topology = irregular_tensorproduct_grid(2, 3, false, false)
+    topologies = (grid_topology, r_ts_topology, i_ts_topology)
+    for topology in topologies
         @testset "2×3 element quad mesh with non-periodic boundaries" begin
             @test length(Topologies.vertices(topology)) == 3 * 4
             V = collect(Topologies.vertices(topology))
@@ -288,8 +358,10 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 3, true, true)
-    _, _, ts_topology = tensorproduct_grid(2, 3, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 3, true, true)
+    _, _, i_ts_topology = irregular_tensorproduct_grid(2, 3, true, true)
+    topologies = (grid_topology, r_ts_topology, i_ts_topology)
+    for topology in topologies
         @testset "2×3 element quad mesh with periodic boundaries" begin
             @test length(Topologies.vertices(topology)) == 2 * 3
             V = collect(Topologies.vertices(topology))
@@ -307,8 +379,9 @@ end
 @testset "simple rectangular mesh coordinates" begin
 
     _, _, grid_topology = rectangular_grid(1, 1, true, true)
-    _, _, ts_topology = tensorproduct_grid(1, 1, true, true)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(1, 1, true, true)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with all periodic boundries" begin
             c1, c2, c3, c4 = Topologies.vertex_coordinates(topology, 1)
             @test c1 == Cartesian2DPoint(0.0, 0.0)
@@ -329,7 +402,7 @@ end
         x2min = -1.0,
         x2max = 1.0,
     )
-    _, _, ts_topology = tensorproduct_grid(
+    _, _, r_ts_topology = regular_tensorproduct_grid(
         1,
         1,
         false,
@@ -339,7 +412,8 @@ end
         x2min = -1.0,
         x2max = 1.0,
     )
-    for topology in (grid_topology, ts_topology)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "1×1 element quad mesh with non-periodic boundries" begin
             c1, c2, c3, c4 = Topologies.vertex_coordinates(topology, 1)
             @test c1 == Cartesian2DPoint(-1.0, -1.0)
@@ -350,8 +424,9 @@ end
     end
 
     _, _, grid_topology = rectangular_grid(2, 4, false, false)
-    _, _, ts_topology = tensorproduct_grid(2, 4, false, false)
-    for topology in (grid_topology, ts_topology)
+    _, _, r_ts_topology = regular_tensorproduct_grid(2, 4, false, false)
+    topologies = (grid_topology, r_ts_topology)
+    for topology in topologies
         @testset "2×4 element quad mesh with non-periodic boundaries" begin
             c1, c2, c3, c4 = Topologies.vertex_coordinates(topology, 1)
             @test c1 == Cartesian2DPoint(0.0, 0.0)
@@ -366,6 +441,20 @@ end
             @test c4 == Cartesian2DPoint(1.0, 1.0)
         end
     end
+    _, _, i_ts_topology = irregular_tensorproduct_grid(2, 4, false, false)
+    @testset "2×4 element quad mesh with non-periodic boundaries" begin
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(i_ts_topology, 1)
+        @test c1 == Cartesian2DPoint(0.0, 0.0)
+        @test c2 == Cartesian2DPoint(0.5, 0.0)
+        @test c3 == Cartesian2DPoint(0.0, 0.375)
+        @test c4 == Cartesian2DPoint(0.5, 0.375)
+
+        c1, c2, c3, c4 = Topologies.vertex_coordinates(i_ts_topology, 8)
+        @test c1 == Cartesian2DPoint(0.5, 0.875)
+        @test c2 == Cartesian2DPoint(1.0, 0.875)
+        @test c3 == Cartesian2DPoint(0.5, 1.0)
+        @test c4 == Cartesian2DPoint(1.0, 1.0)
+    end
 
     _, _, grid_topology = rectangular_grid(
         3,
@@ -377,7 +466,7 @@ end
         x2min = big(0.0),
         x2max = big(1.0),
     )
-    _, _, ts_topology = tensorproduct_grid(
+    _, _, r_ts_topology = regular_tensorproduct_grid(
         3,
         1,
         false,
@@ -387,7 +476,19 @@ end
         x2min = big(0.0),
         x2max = big(1.0),
     )
-    for topology in (grid_topology, ts_topology)
+
+    _, _, i_ts_topology = irregular_tensorproduct_grid(
+        3,
+        1,
+        false,
+        false,
+        x1min = big(0.0),
+        x1max = big(1.0),
+        x2min = big(0.0),
+        x2max = big(1.0),
+    )
+    topologies = (grid_topology, r_ts_topology, i_ts_topology)
+    for topology in topologies
         @testset "check coordinate type accuracy" begin
             c1, c2, c3, c4 = Topologies.vertex_coordinates(topology, 1)
             @test eltype(c2) == BigFloat
