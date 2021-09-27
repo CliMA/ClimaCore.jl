8ba3a41c9357db8285e7804ad7a2eca9753a1982
Shuhei Kadowaki <aviatesk@gmail.com>
2021-09-28 01:55:58 +0900

    update JET
    From Julia v1.7 and higher, JETTest's functionalities are all moved to
    JET.jl (and JETTest is just deprecated).
    Now JET@0.5.0 is released, and let's just focus on running `JET.@test_opt`
    on Julia v1.7 and higher.

--- test/recursive.jl
+++ test/recursive.jl
@@ -1,18 +1,22 @@
-using Test, JETTest
+using JET
 
-using ClimaCore.RecursiveApply
+@static if @isdefined(var"@test_opt") # v1.7 and higher
 
-if VERSION < v"1.7.0-beta1"
-for x in [
-    1.0,
-    1.0f0,
-    (1.0, 2.0),
-    (1.0f0, 2.0f0),
-    (a = 1.0, b = (x1 = 2.0, x2 = 3.0)),
-    (a = 1.0f0, b = (x1 = 2.0f0, x2 = 3.0f0)),
-]
-    @test_nodispatch 2 ⊠ x
-    @test_nodispatch x ⊞ x
-    @test_nodispatch RecursiveApply.rdiv(x, 3)
+using Test, ClimaCore.RecursiveApply
+
+@testset "RecursiveApply optimization test" begin
+    for x in [
+        1.0,
+        1.0f0,
+        (1.0, 2.0),
+        (1.0f0, 2.0f0),
+        (a = 1.0, b = (x1 = 2.0, x2 = 3.0)),
+        (a = 1.0f0, b = (x1 = 2.0f0, x2 = 3.0f0)),
+    ]
+        @test_opt 2 ⊠ x
+        @test_opt x ⊞ x
+        @test_opt RecursiveApply.rdiv(x, 3)
+    end
 end
-end
+
+end # @static if @isdefined(var"@test_opt")
