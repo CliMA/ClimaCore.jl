e09bf37086506710fa3ca5be4c823fc1674869eb
Ben Mackay <jbmackay@caltech.edu>
2021-11-29 13:20:04 -0800

    Add 1D FV remapping.

--- src/Operators/remapping.jl
+++ src/Operators/remapping.jl
@@ -1,6 +1,8 @@
-using ..Spaces: AbstractSpace, SpectralElementSpace2D, Quadratures
-using ..Topologies: GridTopology
+using ..Spaces:
+    AbstractSpace, SpectralElementSpace1D, SpectralElementSpace2D, Quadratures
+using ..Topologies: GridTopology, IntervalTopology
 using ..Fields: Field
+using ..DataLayouts
 using SparseArrays, LinearAlgebra
 
 struct LinearRemap{T <: AbstractSpace, S <: AbstractSpace, M <: AbstractMatrix}
@@ -55,12 +57,34 @@ function linear_remap_op(target::AbstractSpace, source::AbstractSpace)
 end
 
 """
+    overlap_weights(target::T, source::S) where {T <: SpectralElementSpace1D{<:GridTopology, Quadratures.GL{1}},
+            S <: SpectralElementSpace1D{<:GridTopology, Quadratures.GL{1}}}
+
+Computes local weights of the overlap mesh for `source_topo` to `target_topo` topologies.
+
+Returns sparse matrix `W` where entry `W_{ij}` gives the area that target element `i` overlaps source element `j`.
+"""
+function overlap_weights(
+    target::T,
+    source::S,
+) where {
+    T <: SpectralElementSpace1D{<:IntervalTopology, Quadratures.GL{1}},
+    S <: SpectralElementSpace1D{<:IntervalTopology, Quadratures.GL{1}},
+}
+    # Calculate element overlap pattern
+    # X_ov[i,j] = overlap length between target elem i and source elem j
+    X_ov = fv_x_overlap(target, source)
+
+    return X_ov
+end
+
+"""
     overlap_weights(target::T, source::S) where {T <: SpectralElementSpace2D{<:GridTopology, Quadratures.GL{1}},
             S <: SpectralElementSpace2D{<:GridTopology, Quadratures.GL{1}}}
 
 Computes local weights of the overlap mesh for `source_topo` to `target_topo` topologies.
 
-Entry `W_{ij}` gives the area that target element `i` overlaps source element `j`.
+Returns sparse matrix `W` where entry `W_{ij}` gives the area that target element `i` overlaps source element `j`.
 """
 function overlap_weights(
     target::T,
@@ -69,44 +93,87 @@ function overlap_weights(
     T <: SpectralElementSpace2D{<:GridTopology, Quadratures.GL{1}},
     S <: SpectralElementSpace2D{<:GridTopology, Quadratures.GL{1}},
 }
-    target_topo = Spaces.topology(target)
-    source_topo = Spaces.topology(source)
-    nx1, ny1 = target_topo.mesh.n1, target_topo.mesh.n2
-    nx2, ny2 = source_topo.mesh.n1, source_topo.mesh.n2
-
-    X_ov = spzeros(nx1, nx2)
-    Y_ov = spzeros(ny1, ny2)
-
     # Calculate element overlap pattern in x-dimension
     # X_ov[i,j] = overlap length along x-dimension between target elem i and source elem j
-    for i in 1:nx1
-        vertices_i = Topologies.vertex_coordinates(target_topo, i)
-        xmin_i, xmax_i = vertices_i[1].x, vertices_i[2].x
-        for j in 1:nx2
-            vertices_j = Topologies.vertex_coordinates(source_topo, j)
-            xmin_j, xmax_j = vertices_j[1].x, vertices_j[2].x
-            xmin_ov, xmax_ov = max(xmin_i, xmin_j), min(xmax_i, xmax_j)
-            x_overlap = xmax_ov > xmin_ov ? xmax_ov - xmin_ov : continue
-            X_ov[i, j] = x_overlap
-        end
-    end
+    X_ov = fv_x_overlap(target, source)
 
     # Calculate element overlap pattern in y-dimension
-    for i in 1:ny1
-        vertices_i =
-            Topologies.vertex_coordinates(target_topo, 1 + (i - 1) * nx1)
-        ymin_i, ymax_i = vertices_i[1].y, vertices_i[4].y
-        for j in 1:ny2
-            vertices_j =
-                Topologies.vertex_coordinates(source_topo, 1 + (j - 1) * nx2)
-            ymin_j, ymax_j = vertices_j[1].y, vertices_j[4].y
-            ymin_ov, ymax_ov = max(ymin_i, ymin_j), min(ymax_i, ymax_j)
-            y_overlap = ymax_ov > ymin_ov ? ymax_ov - ymin_ov : continue
-            Y_ov[i, j] = y_overlap
-        end
-    end
+    Y_ov = fv_y_overlap(target, source)
+
     return kron(Y_ov, X_ov)
 end
+
+function fv_x_overlap(
+    target::T,
+    source::S,
+) where {
+    T <: Union{SpectralElementSpace1D, SpectralElementSpace2D},
+    S <: Union{SpectralElementSpace1D, SpectralElementSpace2D},
+}
+    target_topo = Spaces.topology(target)
+    source_topo = Spaces.topology(source)
+    ntarget, nsource = nxelems(target_topo), nxelems(source_topo)
+    v1, v2 = 1, 2
+
+    W_ov = spzeros(ntarget, nsource)
+    for i in 1:ntarget
+        vertices_i = Topologies.vertex_coordinates(target_topo, i)
+        min_i, max_i = xcomponent(vertices_i[v1]), xcomponent(vertices_i[v2])
+        for j in 1:nsource
+            vertices_j = Topologies.vertex_coordinates(source_topo, j)
+            min_j, max_j =
+                xcomponent(vertices_j[v1]), xcomponent(vertices_j[v2])
+            min_ov, max_ov = max(min_i, min_j), min(max_i, max_j)
+            overlap_length = max_ov > min_ov ? max_ov - min_ov : continue
+            W_ov[i, j] = overlap_length
+        end
+    end
+    return W_ov
+end
+
+function fv_y_overlap(
+    target::T,
+    source::S,
+) where {
+    T <: Union{SpectralElementSpace1D, SpectralElementSpace2D},
+    S <: Union{SpectralElementSpace1D, SpectralElementSpace2D},
+}
+    target_topo = Spaces.topology(target)
+    source_topo = Spaces.topology(source)
+    ntarget, nsource = nyelems(target_topo), nyelems(source_topo)
+    nx_target, nx_source = nxelems(target_topo), nxelems(source_topo)
+    elem_idx = (i, n) -> 1 + (i - 1) * n
+    v1, v2 = 1, 4
+
+    W_ov = spzeros(ntarget, nsource)
+    for i in 1:ntarget
+        vertices_i =
+            Topologies.vertex_coordinates(target_topo, elem_idx(i, nx_target))
+        min_i, max_i = ycomponent(vertices_i[v1]), ycomponent(vertices_i[v2])
+        for j in 1:nsource
+            vertices_j = Topologies.vertex_coordinates(
+                source_topo,
+                elem_idx(j, nx_source),
+            )
+            min_j, max_j =
+                ycomponent(vertices_j[v1]), ycomponent(vertices_j[v2])
+            min_ov, max_ov = max(min_i, min_j), min(max_i, max_j)
+            overlap_length = max_ov > min_ov ? max_ov - min_ov : continue
+            W_ov[i, j] = overlap_length
+        end
+    end
+    return W_ov
+end
+
+nxelems(topology::Topologies.IntervalTopology) =
+    Topologies.nlocalelems(topology)
+nxelems(topology::Topologies.GridTopology) = topology.mesh.n1
+nyelems(topology::Topologies.GridTopology) = topology.mesh.n2
+
+xcomponent(x::Geometry.XPoint) = Geometry.component(x, 1)
+xcomponent(xy::Geometry.XYPoint) = Geometry.component(xy, 1)
+ycomponent(y::Geometry.YPoint) = Geometry.component(y, 1)
+ycomponent(xy::Geometry.XYPoint) = Geometry.component(xy, 2)
 
 """
     local_weights(space::AbstractSpace)
@@ -123,7 +190,10 @@ function local_weights(space::AbstractSpace)
     n = length(wj)
     J = zeros(FT, n, 1)
     for i in 1:n
-        J[i] = slab(wj, i)[1, 1]
+        J[i] = slab_value(wj, i)
     end
     return J
 end
+
+slab_value(data::DataLayouts.IJFH, i) = slab(data, i)[1, 1]
+slab_value(data::DataLayouts.IFH, i) = slab(data, i)[1]
