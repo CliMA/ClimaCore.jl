d8a661398daac81e78cf2c925c5599f0e062639a
Jia He <jiahe.gatech@gmail.com>
2022-01-11 15:22:18 -0800

    init cond: discrete hydrostatic balance

--- examples/3dsphere/solid_body_rotation_3d.jl
+++ examples/3dsphere/solid_body_rotation_3d.jl
@@ -20,6 +20,10 @@ import Logging
 import TerminalLoggers
 Logging.global_logger(TerminalLoggers.TerminalLogger())
 
+const n_vert = 10
+const n_horz = 4
+const p_horz = 4
+
 const R = 6.4e6 # radius
 const Ω = 7.2921e-5 # Earth rotation (radians / sec)
 const z_top = 3.0e4 # height position of the model top
@@ -30,6 +34,7 @@ const R_d = 287.058 # R dry (gas constant / mol mass dry air)
 const T_tri = 273.16 # triple point temperature
 const γ = 1.4 # heat capacity ratio
 const cv_d = R_d / (γ - 1)
+const cp_d = R_d + cv_d
 
 const T_0 = 300 # isothermal atmospheric temperature
 const H = R_d * T_0 / grav # scale height
@@ -79,9 +84,7 @@ function init_sbr_thermo(z)
     p = p_0 * exp(-z / H)
     ρ = 1 / R_d / T_0 * p
 
-    T = p / ρ / R_d
-
-    e = cv_d * (T - T_tri) + Φ(z)
+    e = cv_d * (T_0 - T_tri) + Φ(z)
     ρe = ρ * e
 
     return (ρ = ρ, ρe = ρe)
@@ -198,7 +201,8 @@ function rhs!(dY, Y, _, t)
 end
 
 # set up 3D spherical domain and coords
-hv_center_space, hv_face_space = sphere_3D()
+hv_center_space, hv_face_space =
+    sphere_3D(R, (0, z_top), n_horz, n_vert, p_horz)
 c_coords = Fields.coordinate_field(hv_center_space)
 f_coords = Fields.coordinate_field(hv_face_space)
 
@@ -207,8 +211,45 @@ const f = @. Geometry.Contravariant3Vector(
     Geometry.WVector(2 * Ω * sind(c_coords.lat)),
 )
 
-# set up initial condition
+# discrete hydrostatic profile
+zc_vec = parent(c_coords.z) |> unique
+
+N = length(zc_vec)
+ρ = zeros(Float64, N)
+p = zeros(Float64, N)
+ρe = zeros(Float64, N)
+
+# compute ρ, ρe, p from analytical formula; not discretely balanced
+for i in 1:N
+    var = init_sbr_thermo(zc_vec[i])
+    ρ[i] = var.ρ
+    ρe[i] = var.ρe
+    p[i] = pressure(ρ[i], ρe[i] / ρ[i], 0.0, zc_vec[i])
+end
+
+ρ_ana = copy(ρ) # keep a copy for analytical ρ which will be used in correction ρe
+
+function discrete_hydrostatic_balance!(ρ, p, dz, grav)
+    for i in 1:(length(ρ) - 1)
+        ρ[i + 1] = -ρ[i] - 2 * (p[i + 1] - p[i]) / dz / grav
+    end
+end
+
+discrete_hydrostatic_balance!(ρ, p, z_top / n_vert, grav)
+# now ρ (after correction) and p (computed from analytical relation) are in discrete hydrostatic balance
+# only need to correct ρe without changing ρ and p, i.e., keep ρT unchanged before vs after the correction on ρ 
+ρe = @. ρe + (ρ - ρ_ana) * Φ(zc_vec) - (ρ - ρ_ana) * cv_d * T_tri
+
+# Note: In princile, ρe = @. cv_d * p /R_d - ρ * cv_d * T_tri + ρ * Φ(zc_vec) should work, 
+#       however, it is not as accurate as the above correction
+
+# set up initial condition: not discretely balanced; only create a Field as a place holder
 Yc = map(coord -> init_sbr_thermo(coord.z), c_coords)
+# put the dicretely balanced ρ and ρe into Yc
+parent(Yc.ρ) .= ρ  # Yc.ρ is a VIJFH layout
+parent(Yc.ρe) .= ρe
+
+# initialize velocity: at rest
 uₕ = map(_ -> Geometry.Covariant12Vector(0.0, 0.0), c_coords)
 w = map(_ -> Geometry.Covariant3Vector(0.0), f_coords)
 Y = Fields.FieldVector(Yc = Yc, uₕ = uₕ, w = w)
@@ -241,5 +282,6 @@ w_phy = Geometry.transform.(Ref(Geometry.WAxis()), sol.u[end].w)
 
 @test maximum(abs.(uₕ_phy.components.data.:1)) ≤ 1e-11
 @test maximum(abs.(uₕ_phy.components.data.:2)) ≤ 1e-11
+@test maximum(abs.(w_phy |> parent)) ≤ 1e-11
 @test norm(sol.u[end].Yc.ρ) ≈ norm(sol.u[1].Yc.ρ) rtol = 1e-2
 @test norm(sol.u[end].Yc.ρe) ≈ norm(sol.u[1].Yc.ρe) rtol = 1e-2
