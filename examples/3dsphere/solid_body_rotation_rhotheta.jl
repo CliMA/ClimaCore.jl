938d38058f5cefe361f99dc41f17881d426c910d
Jia He <jiahe.gatech@gmail.com>
2022-01-11 15:22:18 -0800

    discrete hydrostatic balance

---
+++ examples/3dsphere/solid_body_rotation_rhotheta.jl
@@ -0,0 +1,271 @@
+using Test
+using LinearAlgebra
+
+import ClimaCore:
+    ClimaCore,
+    slab,
+    Spaces,
+    Domains,
+    Meshes,
+    Geometry,
+    Topologies,
+    Spaces,
+    Fields,
+    Operators
+import ClimaCore.Utilities: half
+
+using OrdinaryDiffEq: ODEProblem, solve, SSPRK33
+
+import Logging
+import TerminalLoggers
+Logging.global_logger(TerminalLoggers.TerminalLogger())
+
+const R = 6.4e6 # radius
+const Ω = 7.2921e-5 # Earth rotation (radians / sec)
+const z_top = 3.0e4 # height position of the model top
+const grav = 9.8 # gravitational constant
+const p_0 = 1e5 # mean sea level pressure
+
+const R_d = 287.058 # R dry (gas constant / mol mass dry air)
+const T_tri = 273.16 # triple point temperature
+const γ = 1.4 # heat capacity ratio
+const cv_d = R_d / (γ - 1)
+const cp_d = R_d + cv_d
+
+const T_0 = 300 # isothermal atmospheric temperature
+const H = R_d * T_0 / grav # scale height
+
+# set up function space
+function sphere_3D(
+    R = 6.4e6,
+    zlim = (0, 30.0e3),
+    helem = 4,
+    zelem = 15,
+    npoly = 5,
+)
+    FT = Float64
+    vertdomain = Domains.IntervalDomain(
+        Geometry.ZPoint{FT}(zlim[1]),
+        Geometry.ZPoint{FT}(zlim[2]);
+        boundary_names = (:bottom, :top),
+    )
+    vertmesh = Meshes.IntervalMesh(vertdomain, nelems = zelem)
+    vert_center_space = Spaces.CenterFiniteDifferenceSpace(vertmesh)
+
+    horzdomain = Domains.SphereDomain(R)
+    horzmesh = Meshes.EquiangularCubedSphere(horzdomain, helem)
+    horztopology = Topologies.Topology2D(horzmesh)
+    quad = Spaces.Quadratures.GLL{npoly + 1}()
+    horzspace = Spaces.SpectralElementSpace2D(horztopology, quad)
+
+    hv_center_space =
+        Spaces.ExtrudedFiniteDifferenceSpace(horzspace, vert_center_space)
+    hv_face_space = Spaces.FaceExtrudedFiniteDifferenceSpace(hv_center_space)
+    return (hv_center_space, hv_face_space)
+end
+
+# geopotential
+Φ(z) = grav * z
+
+# pressure
+function pressure(ρ, e, normuvw, z)
+    I = e - Φ(z) - normuvw^2 / 2
+    T = I / cv_d + T_tri
+    return ρ * R_d * T
+end
+
+# initial conditions for density and total energy
+function init_sbr_thermo(z)
+
+    p = p_0 * exp(-z / H)
+    ρ = 1 / R_d / T_0 * p
+
+    θ = T_0 * (p_0/p)^(R_d/cp_d)
+
+    ρθ = ρ * θ
+
+    return (ρ = ρ, ρθ = ρθ)
+end
+
+function rhs!(dY, Y, _, t)
+    cρ = Y.Yc.ρ # density on centers
+    fw = Y.w # Covariant3Vector on faces
+    cuₕ = Y.uₕ # Covariant12Vector on centers
+    cρe = Y.Yc.ρe # total energy on centers
+
+    dρ = dY.Yc.ρ
+    dw = dY.w
+    duₕ = dY.uₕ
+    dρe = dY.Yc.ρe
+
+    # # 0) update w at the bottom
+    # fw = -g^31 cuₕ/ g^33 ????????
+
+    hdiv = Operators.Divergence()
+    hwdiv = Operators.Divergence()
+    hgrad = Operators.Gradient()
+    hwgrad = Operators.Gradient()
+    hcurl = Operators.Curl()
+    hwcurl = Operators.Curl() # Operator.WeakCurl()
+
+    dρ .= 0 .* cρ
+    dw .= 0 .* fw
+    duₕ .= 0 .* cuₕ
+    dρe .= 0 .* cρe
+
+    # hyperdiffusion not needed in SBR
+
+    # 1) Mass conservation
+    If2c = Operators.InterpolateF2C()
+    Ic2f = Operators.InterpolateC2F(
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
+    )
+    cw = If2c.(fw)
+    cuvw = Geometry.Covariant123Vector.(cuₕ) .+ Geometry.Covariant123Vector.(cw)
+
+    # 1.a) horizontal divergence
+    dρ .-= hdiv.(cρ .* (cuvw))
+
+    # 1.b) vertical divergence
+    vdivf2c = Operators.DivergenceF2C(
+        top = Operators.SetValue(Geometry.Contravariant3Vector(0.0)),
+        bottom = Operators.SetValue(Geometry.Contravariant3Vector(0.0)),
+    )
+    # we want the total u³ at the boundary to be zero: we can either constrain
+    # both to be zero, or allow one to be non-zero and set the other to be its
+    # negation
+
+    # explicit part
+    dρ .-= vdivf2c.(Ic2f.(cρ .* cuₕ))
+    # implicit part
+    dρ .-= vdivf2c.(Ic2f.(cρ) .* fw)
+
+    # 2) Momentum equation
+
+    # curl term
+    # effectively a homogeneous Dirichlet condition on u₁ at the boundary
+    vcurlc2f = Operators.CurlC2F(
+        bottom = Operators.SetCurl(Geometry.Contravariant12Vector(0.0, 0.0)),
+        top = Operators.SetCurl(Geometry.Contravariant12Vector(0.0, 0.0)),
+    )
+    cω³ = hcurl.(cuₕ) # Contravariant3Vector
+    fω¹² = hcurl.(fw) # Contravariant12Vector
+    fω¹² .+= vcurlc2f.(cuₕ) # Contravariant12Vector
+
+    # cross product
+    # convert to contravariant
+    # these will need to be modified with topography
+    fu¹² =
+        Geometry.Contravariant12Vector.(
+            Geometry.Covariant123Vector.(Ic2f.(cuₕ)),
+        ) # Contravariant12Vector in 3D
+    fu³ = Geometry.Contravariant3Vector.(Geometry.Covariant123Vector.(fw))
+    @. dw -= fω¹² × fu¹² # Covariant3Vector on faces
+    @. duₕ -= If2c(fω¹² × fu³)
+
+    # Needed for 3D:
+    @. duₕ -=
+        (f + cω³) ×
+        Geometry.Contravariant12Vector(Geometry.Covariant123Vector(cuₕ))
+
+    ce = @. cρe / cρ
+    cp = @. pressure(cρ, ce, norm(cuvw), c_coords.z)
+
+    @. duₕ -= hgrad(cp) / cρ
+    vgradc2f = Operators.GradientC2F(
+        bottom = Operators.SetGradient(Geometry.Covariant3Vector(0.0)),
+        top = Operators.SetGradient(Geometry.Covariant3Vector(0.0)),
+    )
+    @. dw -= vgradc2f(cp) / Ic2f(cρ)
+
+    cE = @. (norm(cuvw)^2) / 2 + Φ(c_coords.z)
+    @. duₕ -= hgrad(cE)
+    @. dw -= vgradc2f(cE)
+
+    # 3) total energy
+
+    @. dρe -= hdiv(cuvw * (cρe + cp))
+    @. dρe -= vdivf2c(fw * Ic2f(cρe + cp))
+    @. dρe -= vdivf2c(Ic2f(cuₕ * (cρe + cp)))
+
+    Spaces.weighted_dss!(dY.Yc)
+    Spaces.weighted_dss!(dY.uₕ)
+    Spaces.weighted_dss!(dY.w)
+
+    return dY
+
+end
+
+# set up 3D spherical domain and coords
+hv_center_space, hv_face_space = sphere_3D()
+c_coords = Fields.coordinate_field(hv_center_space)
+f_coords = Fields.coordinate_field(hv_face_space)
+
+# Coriolis
+const f = @. Geometry.Contravariant3Vector(
+    Geometry.WVector(2 * Ω * sind(c_coords.lat)),
+)
+
+# discrete hydrostatic profile
+zc_vec = parent(c_coords.z) |> unique
+
+N = length(zc_vec)
+ρ = zeros(Float64, N)
+w = zeros(Float64, N+1)
+ρθ = zeros(Float64, N)
+
+for i = 1:N
+    var = init_sbr_thermo(zc_vec[i])
+    ρ[i]  = var.ρ
+    ρθ[i] = var.ρθ
+end
+
+Π(ρθ) = cp_d * (R_d * ρθ / p_0)^(R_d / cv_d)
+function discrete_hydrostatic_balance!(ρ, w, ρθ, Δz::FT, _grav::FT, Π::Function)
+    for i in 1:(length(ρ) - 1)
+        ρ[i + 1] =
+            ρθ[i + 1] /
+            (-2 * _grav / ((Π(ρθ[i + 1]) - Π(ρθ[i])) / Δz) - ρθ[i] / ρ[i])
+    end
+end
+
+discrete_hydrostatic_balance!(ρ, w, ρθ, 30e3/15.0, grav, Π)
+
+
+# set up initial condition
+Yc = map(coord -> init_sbr_thermo(coord.z), c_coords)
+# uₕ = map(_ -> Geometry.Covariant12Vector(0.0, 0.0), c_coords)
+# w = map(_ -> Geometry.Covariant3Vector(0.0), f_coords)
+# Y = Fields.FieldVector(Yc = Yc, uₕ = uₕ, w = w)
+
+# # initialize tendency
+# dYdt = similar(Y)
+# # set up rhs
+# rhs!(dYdt, Y, nothing, 0.0)
+
+# # run!
+# using OrdinaryDiffEq
+# # Solve the ODE
+# T = 3600
+# dt = 5
+# prob = ODEProblem(rhs!, Y, (0.0, T))
+
+# # solve ode
+# sol = solve(
+#     prob,
+#     SSPRK33(),
+#     dt = dt,
+#     saveat = dt,
+#     progress = true,
+#     adaptive = false,
+#     progress_message = (dt, u, p, t) -> t,
+# )
+
+# uₕ_phy = Geometry.transform.(Ref(Geometry.UVAxis()), sol.u[end].uₕ)
+# w_phy = Geometry.transform.(Ref(Geometry.WAxis()), sol.u[end].w)
+
+# @test maximum(abs.(uₕ_phy.components.data.:1)) ≤ 1e-11
+# @test maximum(abs.(uₕ_phy.components.data.:2)) ≤ 1e-11
+# @test norm(sol.u[end].Yc.ρ) ≈ norm(sol.u[1].Yc.ρ) rtol = 1e-2
+# @test norm(sol.u[end].Yc.ρe) ≈ norm(sol.u[1].Yc.ρe) rtol = 1e-2
---
+++ examples/column/hydrostatic_discrete.jl
@@ -0,0 +1,222 @@
+import ClimaCore:
+    Fields,
+    Domains,
+    Topologies,
+    Meshes,
+    DataLayouts,
+    Operators,
+    Geometry,
+    Spaces
+
+using OrdinaryDiffEq: OrdinaryDiffEq, ODEProblem, solve, SSPRK33
+
+import Logging
+import TerminalLoggers
+Logging.global_logger(TerminalLoggers.TerminalLogger())
+const FT = Float64
+
+# https://github.com/CliMA/CLIMAParameters.jl/blob/master/src/Planet/planet_parameters.jl#L5
+const MSLP = FT(1e5) # mean sea level pressure
+const grav = FT(9.8) # gravitational constant
+const R_d = FT(287.058) # R dry (gas constant / mol mass dry air)
+const γ = FT(1.4) # heat capacity ratio
+const C_p = FT(R_d * γ / (γ - 1)) # heat capacity at constant pressure
+const C_v = FT(R_d / (γ - 1)) # heat capacit at constant volume
+const R_m = FT(R_d) # moist R, assumed to be dry
+
+
+domain = Domains.IntervalDomain(
+    Geometry.ZPoint{FT}(0.0),
+    Geometry.ZPoint{FT}(30e3),
+    boundary_names = (:bottom, :top),
+)
+#mesh = Meshes.IntervalMesh(domain, Meshes.ExponentialStretching(7.5e3); nelems = 30)
+mesh = Meshes.IntervalMesh(domain; nelems = 30)
+
+cspace = Spaces.CenterFiniteDifferenceSpace(mesh)
+fspace = Spaces.FaceFiniteDifferenceSpace(cspace)
+
+# https://github.com/CliMA/Thermodynamics.jl/blob/main/src/TemperatureProfiles.jl#L115-L155
+# https://clima.github.io/Thermodynamics.jl/dev/TemperatureProfiles/#DecayingTemperatureProfile
+function decaying_temperature_profile(
+    z;
+    T_virt_surf = FT(280.0),
+    T_min_ref = FT(230.0),
+)
+    # Scale height for surface temperature
+    H_sfc = R_d * T_virt_surf / grav
+    H_t = H_sfc
+
+    z′ = z / H_t
+    tanh_z′ = tanh(z′)
+
+    ΔTv = T_virt_surf - T_min_ref
+    Tv = T_virt_surf - ΔTv * tanh_z′
+
+    ΔTv′ = ΔTv / T_virt_surf
+    p =
+        MSLP * exp(
+            (
+                -H_t *
+                (z′ + ΔTv′ * (log(1 - ΔTv′ * tanh_z′) - log(1 + tanh_z′) + z′))
+            ) / (H_sfc * (1 - ΔTv′^2)),
+        )
+    ρ = p / (R_d * Tv)
+    ρθ = ρ * Tv * (MSLP / p)^(R_m / C_p)
+    return (ρ = ρ, ρθ = ρθ)
+end
+
+Π(ρθ) = C_p * (R_d * ρθ / MSLP)^(R_m / C_v)
+Φ(z) = grav * z
+
+function discrete_hydrostatic_balance!(ρ, w, ρθ, Δz::FT, _grav::FT, Π::Function)
+    # compute θ such that
+    #   I(θ)[i+1/2] = -g / ∂f(Π(ρθ))
+    # discretely, then set
+    #   ρ = ρθ/θ
+    for i in 1:(length(ρ) - 1)
+        #  ρ[i+1] = ρθ[i+1]/(-2Δz*_grav/(Π(ρθ[i+1]) - Π(ρθ[i])) - ρθ[i]/ρ[i])
+        ρ[i + 1] =
+            ρθ[i + 1] /
+            (-2 * _grav / ((Π(ρθ[i + 1]) - Π(ρθ[i])) / Δz) - ρθ[i] / ρ[i])
+
+    end
+end
+
+zc = Fields.coordinate_field(cspace)
+zc_vec = parent(zc)
+
+N = length(zc_vec)
+ρ = zeros(Float64, N)
+w = zeros(Float64, N+1)
+ρθ = zeros(Float64, N)
+
+for i = 1:N
+    var = decaying_temperature_profile(zc_vec[i]; T_virt_surf = 280.0, T_min_ref = 230.0)
+    ρ[i]  = var.ρ
+    ρθ[i] = var.ρθ
+end
+
+discrete_hydrostatic_balance!(ρ, w, ρθ, 30e3/30.0, grav, Π)
+
+Yc = decaying_temperature_profile.(zc.z)
+parent(Yc.ρ) .= ρ
+parent(Yc.ρθ) .= ρθ
+
+w = Geometry.WVector.(zeros(FT, fspace))
+
+Y_init = copy(Yc)
+w_init = copy(w)
+
+function tendency!(dY, Y, _, t)
+    Yc = Y.Yc
+    w = Y.w
+
+    dYc = dY.Yc
+    dw = dY.w
+
+    If = Operators.InterpolateC2F()
+    ∂ = Operators.DivergenceF2C(
+        bottom = Operators.SetValue(Geometry.WVector(zero(FT))),
+        top = Operators.SetValue(Geometry.WVector(zero(FT))),
+    )
+    ∂f = Operators.GradientC2F()
+    B = Operators.SetBoundaryOperator(
+        bottom = Operators.SetValue(Geometry.WVector(zero(FT))),
+        top = Operators.SetValue(Geometry.WVector(zero(FT))),
+    )
+
+    @. dYc.ρ = -(∂(w * If(Yc.ρ)))
+    @. dYc.ρθ = -(∂(w * If(Yc.ρθ)))
+    @. dw =
+        B(Geometry.WVector(-(If(Yc.ρθ / Yc.ρ) * ∂f(Π(Yc.ρθ))) - ∂f(Φ(zc.z))))
+    return dY
+end
+
+Y = Fields.FieldVector(Yc = Yc, w = w)
+dY = tendency!(similar(Y), Y, nothing, 0.0)
+
+Δt = 1.0
+ndays = 10
+
+# Solve the ODE operator
+prob = ODEProblem(tendency!, Y, (0.0, 60 * 60 * 24 * ndays))
+sol = solve(
+    prob,
+    SSPRK33(),
+    dt = Δt,
+    saveat = 60 * 60, # save every hour
+    progress = true,
+    progress_message = (dt, u, p, t) -> t,
+);
+
+ENV["GKSwstype"] = "nul"
+using ClimaCorePlots, Plots
+Plots.GRBackend()
+
+dirname = "hydrostatic"
+path = joinpath(@__DIR__, "output", dirname)
+mkpath(path)
+
+z_centers = parent(Fields.coordinate_field(cspace))
+z_faces = parent(Fields.coordinate_field(fspace))
+
+function hydrostatic_plot(u; title = "", size = (1024, 600))
+    sub_plt1 = Plots.plot(
+        parent(Y_init.ρ),
+        z_centers,
+        marker = :circle,
+        xlabel = "ρ",
+        label = "T=0",
+    )
+    sub_plt1 = Plots.plot!(sub_plt1, parent(u.Yc.ρ), z_centers, label = "T")
+
+    sub_plt2 = Plots.plot(
+        parent(w_init),
+        z_faces,
+        marker = :circle,
+        xlim = (-0.2, 0.2),
+        xlabel = "ω",
+        label = "T=0",
+    )
+    sub_plt2 = Plots.plot!(sub_plt2, parent(u.w), z_faces, label = "T")
+
+    sub_plt3 = Plots.plot(
+        parent(Y_init.ρθ),
+        z_centers,
+        marker = :circle,
+        xlabel = "ρθ",
+        label = "T=0",
+    )
+    sub_plt3 = Plots.plot!(sub_plt3, parent(u.Yc.ρθ), z_centers, label = "T")
+
+    return Plots.plot(
+        sub_plt1,
+        sub_plt2,
+        sub_plt3,
+        title = title,
+        layout = (1, 3),
+        size = size,
+    )
+end
+
+anim = Plots.@animate for (i, u) in enumerate(sol.u)
+    hydrostatic_plot(u, title = "Hour $(i)")
+end
+Plots.mp4(anim, joinpath(path, "hydrostatic.mp4"), fps = 10)
+
+Plots.png(hydrostatic_plot(sol[end]), joinpath(path, "hydrostatic_end.png"))
+
+function linkfig(figpath, alt = "")
+    # buildkite-agent upload figpath
+    # link figure in logs if we are running on CI
+    if get(ENV, "BUILDKITE", "") == "true"
+        artifact_url = "artifact://$figpath"
+        print("\033]1338;url='$(artifact_url)';alt='$(alt)'\a\n")
+    end
+end
+
+linkfig(
+    "examples/column/output/$(dirname)/hydrostatic_end.png",
+    "Hydrostatic End",
+)
