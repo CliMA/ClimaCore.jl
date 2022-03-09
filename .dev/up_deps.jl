41bb14344b46c1fc4019513b3ba39b2136c83124
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-03-08 16:12:37 -0800

    Add up_deps.jl script to dev folder

---
+++ .dev/up_deps.jl
@@ -0,0 +1,31 @@
+#=
+A simple script for updating the manifest
+files in all of our environments.
+=#
+
+root = dirname(@__DIR__)
+
+dirs = (
+    root,
+    joinpath(root, "examples"),
+    joinpath(root, "perf"),
+    joinpath(root, "docs"),
+    joinpath(root, "lib", "ClimaCoreMakie"),
+    joinpath(root, "lib", "ClimaCorePlots"),
+    joinpath(root, "lib", "ClimaCoreTempestRemap"),
+    joinpath(root, "lib", "ClimaCoreVTK"),
+)
+
+cd(root) do
+    for dir in dirs
+        cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.update()'`
+        run(cmd)
+    end
+end
+
+# https://github.com/JuliaLang/Pkg.jl/issues/3014
+for dir in dirs
+    cd(dir) do
+        rm("LocalPreferences.toml"; force = true)
+    end
+end
