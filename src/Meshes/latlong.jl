3ecaef6b38a8764351795abcddb5472a457c5897
Ben Mackay <jbmackay@caltech.edu>
2022-06-07 17:13:40 -0700

    Change to ranges.

--- src/Meshes/latlong.jl
+++ src/Meshes/latlong.jl
@@ -1,9 +1,23 @@
 using IntervalSets
 
-struct RegularLatLongMesh <: AbstractMesh2D
-    domain::Domains.SphereDomain
-    nlat::Int
-    nlong::Int
+struct RegularLatLongMesh{S <: Domains.SphereDomain, R <: StepRangeLen} <: AbstractMesh2D
+    domain::S
+    lat::R
+    long::R
+end
+
+function RegularLatLongMesh(domain, nlat::Int, nlong::Int, lat_bounds = (-π/2, π/2), long_bounds = (-π, π))
+    @assert 0 < (lat_bounds[2] - lat_bounds[1]) <= π
+    @assert 0 < (long_bounds[2] - long_bounds[1]) <= 2π
+    
+    FT = typeof(domain.radius)
+    lat = range(ClosedInterval{FT}(lat_bounds[1], lat_bounds[2]), nlat) # lat [-π/2, π/2]
+    if long_bounds[1] + 2π ≈ long_bounds[2] # periodic, long_bounds[2] "=" long_bounds[1]
+        long = range(Interval{:closed, :open, FT}(long_bounds[1], long_bounds[2]), nlong) # long (-π, π]
+    else
+        long = range(ClosedInterval{FT}(long_bounds[1], long_bounds[2]), nlong) # long (-π, π]
+    end
+    return RegularLatLongMesh{typeof.((domain, lat))...}(domain, lat, long)
 end
 
 domain(mesh::RegularLatLongMesh) = mesh.domain
@@ -11,14 +25,10 @@ domain(mesh::RegularLatLongMesh) = mesh.domain
 function coordinates_list(
     mesh::RegularLatLongMesh,
 )
-    r = domain(mesh).radius
-    nlat, nlong = mesh.nlat, mesh.nlong
-    FT = typeof(r)
-    θ = range(Interval{:closed, :open}(FT(0), FT(2π)), nlong) # long (0, 2π]
-    ϕ = range(ClosedInterval{FT}(0, π), nlat) # lat [0, π]
+    latitude, longitude = mesh.lat, mesh.long
     coords = []
-    for lat in ϕ
-        for long in θ
+    for lat in latitude
+        for long in longitude
             push!(coords, Geometry.LatLongPoint(lat, long))
         end
     end
