1a2f0006860d5c8ec0cedcece6ea643a3106ccfc
Ben Mackay <jbmackay@caltech.edu>
2022-06-13 16:25:52 -0700

    Change to degrees.

--- src/Meshes/latlong.jl
+++ src/Meshes/latlong.jl
@@ -11,21 +11,21 @@ function RegularLatLongMesh(
     domain,
     nlat::Int,
     nlong::Int,
-    lat_bounds = (-π / 2, π / 2),
-    long_bounds = (-π, π),
+    lat_bounds = (-90, 90),
+    long_bounds = (-180, 180),
 )
-    @assert 0 < (lat_bounds[2] - lat_bounds[1]) <= π
-    @assert 0 < (long_bounds[2] - long_bounds[1]) <= 2π
+    @assert -90 <= lat_bounds[1] < lat_bounds[2] <= 90
+    @assert -180 <= long_bounds[1] < long_bounds[2] <= 180
 
     FT = typeof(domain.radius)
-    lat = range(ClosedInterval{FT}(lat_bounds[1], lat_bounds[2]), nlat) # lat [-π/2, π/2]
-    if long_bounds[1] + 2π ≈ long_bounds[2] # periodic, long_bounds[2] "=" long_bounds[1]
+    lat = range(ClosedInterval{FT}(lat_bounds[1], lat_bounds[2]), nlat) # lat [-90, 90]
+    if long_bounds[1] + 360 ≈ long_bounds[2] # periodic, long_bounds[2] "=" long_bounds[1]
         long = range(
             Interval{:closed, :open, FT}(long_bounds[1], long_bounds[2]),
             nlong,
-        ) # long (-π, π]
+        ) # long (-180, 180]
     else
-        long = range(ClosedInterval{FT}(long_bounds[1], long_bounds[2]), nlong) # long (-π, π]
+        long = range(ClosedInterval{FT}(long_bounds[1], long_bounds[2]), nlong) # long (-180, 180]
     end
     return RegularLatLongMesh{typeof.((domain, lat))...}(domain, lat, long)
 end
