e5d37c2dcc001455cb28d772a7a2c4c49a064dff
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-07-22 11:33:48 -0700

    Only grad benchmark

--- test/Operators/finitedifference/column_benchmark.jl
+++ test/Operators/finitedifference/column_benchmark.jl
@@ -5,7 +5,7 @@ include("column_benchmark_utils.jl")
     benchmark_grad(vars_contig, cfield, ffield)
 end
 
-@testset "Benchmark cases" begin
-    (; cfield, ffield, vars_contig) = get_fields(1000, Float64)
-    benchmark_cases(vars_contig, cfield, ffield)
-end
+# @testset "Benchmark cases" begin
+#     (; cfield, ffield, vars_contig) = get_fields(1000, Float64)
+#     benchmark_cases(vars_contig, cfield, ffield)
+# end
