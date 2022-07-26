193402d41533ef6b5dbb388e1f9b32f09ff4c2d9
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-07-25 17:06:38 -0700

    Expand column perf tests to include hspaces

--- test/Operators/finitedifference/column_benchmark.jl
+++ test/Operators/finitedifference/column_benchmark.jl
@@ -1,8 +1,12 @@
 include("column_benchmark_utils.jl")
 
-@testset "Benchmark cases" begin
-    (; cfield, ffield, vars_contig) = get_fields(1000, Float64)
-    benchmark_cases(vars_contig, cfield, ffield)
+@testset "Benchmark operators" begin
+    # (; cfield, ffield, vars_contig) = get_fields(1000, Float64, false)
+    # benchmark_arrays(vars_contig)
+    # benchmark_operators(cfield, ffield, vars_contig.has_h_space)
+    # (; cfield, ffield, vars_contig) = get_fields(1000, Float64, true)
+    # benchmark_operators(cfield, ffield, vars_contig.has_h_space)
+    benchmark_operators(1000, Float64)
 end
 
 nothing
