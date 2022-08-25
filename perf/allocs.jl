55ad9ab19aa8f55206b220b9f14e9efec1936ca8
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-08-25 12:56:37 -0700

    Remove some allocs jobs

--- perf/allocs.jl
+++ perf/allocs.jl
@@ -20,9 +20,7 @@ dirs_to_monitor = [
 all_cases = [
     (joinpath(EXAMPLE_DIR, "sphere", "shallow_water.jl"), "barotropic_instability", ""),
     (joinpath(EXAMPLE_DIR, "hybrid", "plane", "bubble_2d_rhotheta.jl"), "", ""),
-    (joinpath(EXAMPLE_DIR, "hybrid", "box", "bubble_3d_rhotheta.jl"), "", ""),
     (joinpath(EXAMPLE_DIR, "hybrid", "driver.jl"), "", "sphere/baroclinic_wave_rhoe"),
-    (joinpath(EXAMPLE_DIR, "hybrid", "driver.jl"), "", "sphere/baroclinic_wave_rhotheta"),
 ]
 #! format: on
 
