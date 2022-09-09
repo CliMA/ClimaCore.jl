10abc2e4d5b50644ef599226e84abf73fb830873
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-09-09 13:52:55 -0700

    Add Aqua ambiguities test

--- test/aqua.jl
+++ test/aqua.jl
@@ -7,6 +7,21 @@ using Aqua
     # https://github.com/JuliaLang/julia/issues/29393
     Aqua.test_unbound_args(ClimaCore)
 
+    # See: https://github.com/SciML/OrdinaryDiffEq.jl/issues/1750
+    # Test that we're not introducing method ambiguities across deps
+    ambs = Aqua.detect_ambiguities(ClimaCore; recursive = true)
+    pkg_match(pkgname, pkdir::Nothing) = false
+    pkg_match(pkgname, pkdir::AbstractString) = occursin(pkgname, pkdir)
+    filter!(x -> pkg_match("ClimaCore", pkgdir(last(x).module)), ambs)
+    for method_ambiguity in ambs
+        @show method_ambiguity
+    end
+    # If the number of ambiguities is less than the limit below,
+    # then please lower the limit based on the new number of ambiguities.
+    # We're trying to drive this number down to zero to reduce latency.
+    @info "Number of method ambiguities: $(length(ambs))"
+    @test length(ambs) ≤ 129
+
     # returns a vector of all unbound args
     # ua = Aqua.detect_unbound_args_recursively(ClimaCore)
 end
