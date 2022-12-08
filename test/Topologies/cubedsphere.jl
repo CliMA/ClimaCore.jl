72d3c0eabd5e808bc2d4ff19a97a05a78fa2cbad
sriharshakandala <sriharsha.kvs@gmail.com>
2022-12-08 10:49:03 -0800

    Use Singleton comms context for serial Topology tests.

--- test/Topologies/cubedsphere.jl
+++ test/Topologies/cubedsphere.jl
@@ -1,16 +1,20 @@
+using ClimaComms
 using ClimaCore: Geometry, Domains, Meshes, Topologies
 using Test
 
+const context = ClimaComms.SingletonCommsContext()
 
 @testset "neighboring element tests" begin
     @testset "1 element across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 1),
         )
         @test Topologies.local_neighboring_elements(topology, 1) == [2, 3, 5, 6]
     end
     @testset "2 elements across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 2),
         )
         @test Topologies.local_neighboring_elements(topology, 1) ==
@@ -23,7 +27,8 @@ using Test
               [1, 2, 3, 5, 7, 9, 11]
     end
     @testset "3 elements across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 3),
         )
         @test Topologies.local_neighboring_elements(topology, 5) ==
@@ -33,13 +38,15 @@ end
 
 @testset "interior faces iterator" begin
     @testset "1 element across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 1),
         )
         @test length(Topologies.interior_faces(topology)) == 12
     end
     @testset "3 elements across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 3),
         )
         @test length(Topologies.interior_faces(topology)) == 6 * 2 * 6 + 12 * 3
@@ -48,13 +55,15 @@ end
 
 @testset "boundaries" begin
     @testset "1 element across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 1),
         )
         @test isempty(Topologies.boundary_tags(topology))
     end
     @testset "3 elements across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 3),
         )
         @test isempty(Topologies.boundary_tags(topology))
@@ -63,7 +72,8 @@ end
 
 @testset "boundaries" begin
     @testset "1 element across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 1),
         )
         length(Topologies.local_vertices(topology)) == 8
@@ -72,7 +82,8 @@ end
         end
     end
     @testset "3 elements across each panel" begin
-        topology = Topologies.Topology2D(
+        topology = Topologies.DistributedTopology2D(
+            context,
             Meshes.EquiangularCubedSphere(Domains.SphereDomain(5.0), 3),
         )
         length(Topologies.local_vertices(topology)) == 8 + 12 * 2 + 6 * 4
