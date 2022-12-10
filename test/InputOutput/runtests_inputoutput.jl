02ede88a7e423f39baee4fbc84e443c708916e5c
Charles Kawczynski <kawczynski.charles@gmail.com>
2022-12-09 18:21:54 -0800

    Revert some test changes

--- test/InputOutput/runtests_inputoutput.jl
+++ test/InputOutput/runtests_inputoutput.jl
@@ -1,23 +1,7 @@
-using SafeTestsets
-
-@safetestset "spectralelement2d" begin
-    @time include("spectralelement2d.jl")
-end
-@safetestset "hybrid2dbox" begin
-    @time include("hybrid2dbox.jl")
-end
-@safetestset "hybrid2dbox_topography" begin
-    @time include("hybrid2dbox_topography.jl")
-end
-@safetestset "hybrid2dbox_stretched" begin
-    @time include("hybrid2dbox_stretched.jl")
-end
-@safetestset "hybrid3dbox" begin
-    @time include("hybrid3dbox.jl")
-end
-@safetestset "hybrid3dcubedsphere" begin
-    @time include("hybrid3dcubedsphere.jl")
-end
-@safetestset "hybrid3dcubedsphere_topography" begin
-    @time include("hybrid3dcubedsphere_topography.jl")
-end
+@time include("spectralelement2d.jl")
+@time include("hybrid2dbox.jl")
+@time include("hybrid2dbox_topography.jl")
+@time include("hybrid2dbox_stretched.jl")
+@time include("hybrid3dbox.jl")
+@time include("hybrid3dcubedsphere.jl")
+@time include("hybrid3dcubedsphere_topography.jl")
