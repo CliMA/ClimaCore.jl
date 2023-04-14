4d2fccb3ebcc489744219683dfd16efb01ff4fb8
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-04-14 12:11:17 -0700

    Slightly increase spec elem benchmark lim buffer

--- test/Operators/spectralelement/benchmark_utils.jl
+++ test/Operators/spectralelement/benchmark_utils.jl
@@ -231,7 +231,7 @@ function tabulate_summary(summary)
 end
 
 function test_against_best_times(bm, best_times)
-    buffer = 1.1
+    buffer = 1.2
     pass(k) = bm[k].t_mean_float < best_times[k] * buffer
     intersect_keys = intersect(collect(keys(bm)), collect(keys(best_times)))
     if !all(k -> pass(k), intersect_keys)
