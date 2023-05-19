19a507eeb9ecf848a057774d50b12f34ecdd6833
sriharshakandala <sriharsha.kvs@gmail.com>
2023-05-19 14:28:09 -0700

    Add CUDA driver for bubble_3d_invariant_rhoe

---
+++ examples/hybrid/box/bubble_3d_invariant_rhoe_cuda.jl
@@ -0,0 +1,367 @@
+using CUDA
+using Test
+using ClimaComms
+using StaticArrays, IntervalSets, LinearAlgebra, UnPack
+
+import ClimaCore:
+    ClimaCore,
+    slab,
+    Spaces,
+    Domains,
+    Meshes,
+    Geometry,
+    Topologies,
+    DataLayouts,
+    Spaces,
+    Fields,
+    Operators
+using ClimaCore.Geometry
+
+device = ClimaComms.device()
+comms_ctx = ClimaComms.SingletonCommsContext(device)
+
+function hvspace_3D(
+    comms_ctx,
+    xlim = (-π, π),
+    ylim = (-π, π),
+    zlim = (0, 4π),
+    xelem = 4,
+    yelem = 4,
+    zelem = 16,
+    npoly = 3;
+)
+    FT = Float64
+    vertdomain = Domains.IntervalDomain(
+        Geometry.ZPoint{FT}(zlim[1]),
+        Geometry.ZPoint{FT}(zlim[2]);
+        boundary_tags = (:bottom, :top),
+    )
+    vertmesh = Meshes.IntervalMesh(vertdomain, nelems = zelem)
+    verttopo = Topologies.IntervalTopology(
+        comms_ctx,
+        vertmesh,
+    )
+    vert_center_space = Spaces.CenterFiniteDifferenceSpace(verttopo)
+
+    horzdomain = Domains.RectangleDomain(
+        Geometry.XPoint{FT}(xlim[1]) .. Geometry.XPoint{FT}(xlim[2]),
+        Geometry.YPoint{FT}(ylim[1]) .. Geometry.YPoint{FT}(ylim[2]),
+        x1periodic = true,
+        x2periodic = true,
+    )
+    Nv = Meshes.nelements(vertmesh)
+    Nf_center, Nf_face = 2, 1 #1 + 3 + 1
+    quad = Spaces.Quadratures.GLL{npoly + 1}()
+    horzmesh = Meshes.RectilinearMesh(horzdomain, xelem, yelem)
+    horztopology = Topologies.Topology2D(comms_ctx, horzmesh)
+    horzspace = Spaces.SpectralElementSpace2D(horztopology, quad)
+
+    hv_center_space =
+        Spaces.ExtrudedFiniteDifferenceSpace(horzspace, vert_center_space)
+    hv_face_space = Spaces.FaceExtrudedFiniteDifferenceSpace(hv_center_space)
+    return (hv_center_space, hv_face_space, horztopology)
+end
+@show "before building spaces"
+# set up 3D domain - doubly periodic box
+hv_center_space, hv_face_space, horztopology =
+    hvspace_3D(comms_ctx, (-500, 500), (-500, 500), (0, 1000))
+@show "after building spaces"
+const MSLP = 1e5 # mean sea level pressure
+const grav = 9.8 # gravitational constant
+const R_d = 287.058 # R dry (gas constant / mol mass dry air)
+const γ = 1.4 # heat capacity ratio
+const C_p = R_d * γ / (γ - 1) # heat capacity at constant pressure
+const C_v = R_d / (γ - 1) # heat capacity at constant volume
+const T_0 = 273.16 # triple point temperature
+
+Φ(z) = grav * z
+
+function pressure(ρ, e, uvw, z)
+    I = e - Φ(z) - (norm(uvw)^2) / 2
+    T = I / C_v + T_0
+    return ρ * R_d * T
+end
+
+# Reference: https://journals.ametsoc.org/view/journals/mwre/140/4/mwr-d-10-05073.1.xml, Section 5a
+function init_dry_rising_bubble_3d(x, y, z)
+    x_c = 0.0
+    y_c = 0.0
+    z_c = 350.0
+    r_c = 250.0
+    θ_b = 300.0
+    θ_c = 0.5
+    cp_d = C_p
+    cv_d = C_v
+    p_0 = MSLP
+    g = grav
+
+    # auxiliary quantities
+    r = sqrt((x - x_c)^2 + (y - y_c)^2 + (z - z_c)^2)
+    θ_p = r < r_c ? 0.5 * θ_c * (1.0 + cospi(r / r_c)) : 0.0 # potential temperature perturbation
+
+    θ = θ_b + θ_p # potential temperature
+    π_exn = 1.0 - g * z / cp_d / θ # exner function
+    T = π_exn * θ # temperature
+    p = p_0 * π_exn^(cp_d / R_d) # pressure
+    ρ = p / R_d / T # density
+    e = cv_d * (T - T_0) + g * z
+    ρe = ρ * e # total energy
+
+    return (ρ = ρ, ρe = ρe)
+end
+
+# initial conditions
+coords = Fields.coordinate_field(hv_center_space)
+face_coords = Fields.coordinate_field(hv_face_space)
+
+Yc = map(coord -> init_dry_rising_bubble_3d(coord.x, coord.y, coord.z), coords)
+uₕ = map(_ -> Geometry.Covariant12Vector(0.0, 0.0), coords)
+w = map(_ -> Geometry.Covariant3Vector(0.0), face_coords)
+Y = Fields.FieldVector(Yc = Yc, uₕ = uₕ, w = w)
+
+energy_0 = sum(Y.Yc.ρe)
+mass_0 = sum(Y.Yc.ρ)
+
+@show (energy_0, mass_0)
+ghost_buffer = (
+    dρe = Spaces.create_dss_buffer(Yc.ρe),
+    duₕ = Spaces.create_dss_buffer(Y.uₕ),
+    uₕ = Spaces.create_dss_buffer(Y.uₕ),
+    Yc = Spaces.create_dss_buffer(Y.Yc),
+    w = Spaces.create_dss_buffer(Y.w),
+)
+function rhs_invariant!(dY, Y, ghost_buffer, t)
+
+    cρ = Y.Yc.ρ # scalar on centers
+    fw = Y.w # Covariant3Vector on faces
+    cuₕ = Y.uₕ # Covariant12Vector on centers
+    cρe = Y.Yc.ρe
+
+    dρ = dY.Yc.ρ
+    dw = dY.w
+    duₕ = dY.uₕ
+    dρe = dY.Yc.ρe
+    z = coords.z
+
+    # 0) update w at the bottom
+    # fw = -g^31 cuₕ/ g^33
+
+    hdiv = Operators.Divergence()
+    hwdiv = Operators.WeakDivergence()
+    hgrad = Operators.Gradient()
+    hwgrad = Operators.WeakGradient()
+    hcurl = Operators.Curl()
+    hwcurl = Operators.WeakCurl()
+
+    dρ .= 0 .* cρ
+
+    If2c = Operators.InterpolateF2C()
+    Ic2f = Operators.InterpolateC2F(
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
+    )
+    cw = If2c.(fw)
+    fuₕ = Ic2f.(cuₕ)
+    cuvw = Geometry.Covariant123Vector.(cuₕ) .+ Geometry.Covariant123Vector.(cw)
+
+    ce = @. cρe / cρ
+    cI = @. ce - Φ(z) - (norm(cuvw)^2) / 2
+    cT = @. cI / C_v + T_0
+    cp = @. cρ * R_d * cT
+
+    ### HYPERVISCOSITY
+    # 1) compute hyperviscosity coefficients
+    ch_tot = @. ce + cp / cρ
+    χe = @. dρe = hwdiv(hgrad(ch_tot)) # we store χe in dρe
+    χuₕ = @. duₕ =
+        hwgrad(hdiv(cuₕ)) - Geometry.Covariant12Vector(
+            hwcurl(Geometry.Covariant3Vector(hcurl(cuₕ))),
+        )
+    Spaces.weighted_dss_start!(dρe, ghost_buffer.dρe)
+    Spaces.weighted_dss_start!(duₕ, ghost_buffer.duₕ)
+    Spaces.weighted_dss_internal!(dρe, ghost_buffer.dρe)
+    Spaces.weighted_dss_internal!(duₕ, ghost_buffer.duₕ)
+    Spaces.weighted_dss_ghost!(dρe, ghost_buffer.dρe)
+    Spaces.weighted_dss_ghost!(duₕ, ghost_buffer.duₕ)
+
+    κ₄ = 100.0 # m^4/s
+    @. dρe = -κ₄ * hwdiv(cρ * hgrad(χe))
+    @. duₕ =
+        -κ₄ * (
+            hwgrad(hdiv(χuₕ)) - Geometry.Covariant12Vector(
+                hwcurl(Geometry.Covariant3Vector(hcurl(χuₕ))),
+            )
+        )
+
+    # 1) Mass conservation
+
+    dw .= fw .* 0
+
+    # 1.a) horizontal divergence
+    dρ .-= hdiv.(cρ .* (cuvw))
+
+    # 1.b) vertical divergence
+    vdivf2c = Operators.DivergenceF2C(
+        top = Operators.SetValue(Geometry.Contravariant3Vector(0.0)),
+        bottom = Operators.SetValue(Geometry.Contravariant3Vector(0.0)),
+    )
+    # we want the total u³ at the boundary to be zero: we can either constrain
+    # both to be zero, or allow one to be non-zero and set the other to be its
+    # negation
+
+    # explicit part
+    dρ .-= vdivf2c.(Ic2f.(cρ .* cuₕ))
+    # implicit part
+    dρ .-= vdivf2c.(Ic2f.(cρ) .* fw)
+
+    # 2) Momentum equation
+
+    # curl term
+    hcurl = Operators.Curl()
+    # effectively a homogeneous Dirichlet condition on u₁ at the boundary
+    vcurlc2f = Operators.CurlC2F(
+        bottom = Operators.SetCurl(Geometry.Contravariant12Vector(0.0, 0.0)),
+        top = Operators.SetCurl(Geometry.Contravariant12Vector(0.0, 0.0)),
+    )
+    cω³ = hcurl.(cuₕ) # Contravariant3Vector
+    fω¹² = hcurl.(fw) # Contravariant12Vector
+    fω¹² .+= vcurlc2f.(cuₕ) # Contravariant12Vector
+
+    # cross product
+    # convert to contravariant
+    # these will need to be modified with topography
+    fu¹² =
+        Geometry.Contravariant12Vector.(
+            Geometry.Covariant123Vector.(Ic2f.(cuₕ)),
+        ) # Contravariant12Vector in 3D
+    fu³ = Geometry.Contravariant3Vector.(Geometry.Covariant123Vector.(fw))
+    @. dw -= fω¹² × fu¹² # Covariant3Vector on faces
+    @. duₕ -= If2c(fω¹² × fu³)
+
+    # Needed for 3D:
+    @. duₕ -=
+        cω³ × Geometry.Contravariant12Vector(Geometry.Covariant123Vector(cuₕ))
+
+
+    @. duₕ -= hgrad(cp) / cρ
+    vgradc2f = Operators.GradientC2F(
+        bottom = Operators.SetGradient(Geometry.Covariant3Vector(0.0)),
+        top = Operators.SetGradient(Geometry.Covariant3Vector(0.0)),
+    )
+    @. dw -= vgradc2f(cp) / Ic2f(cρ)
+
+    cE = @. (norm(cuvw)^2) / 2 + Φ(z)
+    @. duₕ -= hgrad(cE)
+    @. dw -= vgradc2f(cE)
+
+    # 3) potential temperature
+
+    @. dρe -= hdiv(cuvw * (cρe + cp))
+    @. dρe -= vdivf2c(fw * Ic2f(cρe + cp))
+    @. dρe -= vdivf2c(Ic2f(cuₕ * (cρe + cp)))
+
+    fcc = Operators.FluxCorrectionC2C(
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
+    )
+    fcf = Operators.FluxCorrectionF2F(
+        bottom = Operators.Extrapolate(),
+        top = Operators.Extrapolate(),
+    )
+
+    @. dρ += fcc(fw, cρ)
+    @. dρe += fcc(fw, cρe)
+    # dYc.ρuₕ += fcc(w, Yc.ρuₕ)
+
+    Spaces.weighted_dss_start!(dY.Yc, ghost_buffer.Yc)
+    Spaces.weighted_dss_start!(dY.uₕ, ghost_buffer.uₕ)
+    Spaces.weighted_dss_start!(dY.w, ghost_buffer.w)
+    Spaces.weighted_dss_internal!(dY.Yc, ghost_buffer.Yc)
+    Spaces.weighted_dss_internal!(dY.uₕ, ghost_buffer.uₕ)
+    Spaces.weighted_dss_internal!(dY.w, ghost_buffer.w)
+    Spaces.weighted_dss_ghost!(dY.Yc, ghost_buffer.Yc)
+    Spaces.weighted_dss_ghost!(dY.uₕ, ghost_buffer.uₕ)
+    Spaces.weighted_dss_ghost!(dY.w, ghost_buffer.w)
+    return dY
+end
+dYdt = similar(Y);
+rhs_invariant!(dYdt, Y, ghost_buffer, 0.0);
+#=
+# run!
+using OrdinaryDiffEq
+Δt = 0.050
+prob = ODEProblem(rhs_invariant!, Y, (0.0, 700.0), ghost_buffer)
+integrator = OrdinaryDiffEq.init(
+    prob,
+    SSPRK33(),
+    dt = Δt,
+    saveat = 10.0,
+    progress = true,
+    progress_message = (dt, u, p, t) -> t,
+);
+=#
+#if haskey(ENV, "CI_PERF_SKIP_RUN") # for performance analysis
+#    throw(:exit_profile)
+#end
+
+#t_diff = @elapsed sol_invariant = OrdinaryDiffEq.solve!(integrator)
+
+#=
+FT = Float64
+Es = FT[]
+Mass = FT[]
+if usempi
+    for sol_step in sol_invariant.u
+        Es_step = ClimaComms.reduce(comms_ctx, sum(sol_step.Yc.ρe), +)
+        Mass_step = ClimaComms.reduce(comms_ctx, sum(sol_step.Yc.ρ), +)
+        if ClimaComms.iamroot(comms_ctx)
+            push!(Es, Es_step)
+            push!(Mass, Mass_step)
+        end
+    end
+else
+    for sol_step in sol_invariant.u
+        push!(Es, sum(sol_step.Yc.ρe))
+        push!(Mass, sum(sol_step.Yc.ρ))
+    end
+end
+
+ENV["GKSwstype"] = "nul"
+import Plots
+Plots.GRBackend()
+
+dir = "bubble_3d_invariant_rhoe"
+path = joinpath(@__DIR__, "output", dir)
+mkpath(path)
+
+if !usempi || ClimaComms.iamroot(comms_ctx)
+    println("Walltime = $t_diff seconds")
+    # post-processing
+    Plots.png(
+        Plots.plot((Es .- energy_0) ./ energy_0),
+        joinpath(path, "energy.png"),
+    )
+    Plots.png(
+        Plots.plot((Mass .- mass_0) ./ mass_0),
+        joinpath(path, "mass.png"),
+    )
+
+    function linkfig(figpath, alt = "")
+        # buildkite-agent upload figpath
+        # link figure in logs if we are running on CI
+        if get(ENV, "BUILDKITE", "") == "true"
+            artifact_url = "artifact://$figpath"
+            print("\033]1338;url='$(artifact_url)';alt='$(alt)'\a\n")
+        end
+    end
+
+    linkfig(
+        relpath(joinpath(path, "energy.png"), joinpath(@__DIR__, "../..")),
+        "Total Energy",
+    )
+    linkfig(
+        relpath(joinpath(path, "mass.png"), joinpath(@__DIR__, "../..")),
+        "Mass",
+    )
+end
+=#
