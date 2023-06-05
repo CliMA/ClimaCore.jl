8a6f4bc57fafc064015f1aa9c1e6d0927acc664d
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-06-05 14:48:00 -0700

    Add distributed cuda space construction test
    Update .buildkite/pipeline.yml
    
    Try distinct nodes via env variables
    
    Co-authored-by: Simon Byrne <simonbyrne@gmail.com>

---
+++ test/Spaces/distributed_cuda/space_construction.jl
@@ -0,0 +1,42 @@
+using Logging
+using Test
+using ClimaComms
+using CUDA
+
+import ClimaCore:
+    Domains, Fields, Geometry, Meshes, Operators, Spaces, Topologies
+
+@testset "Distributed extruded CUDA space" begin
+    # initializing MPI
+    device = ClimaComms.device()
+    context = ClimaComms.MPICommsContext(device)
+    vcontext = ClimaComms.SingletonCommsContext(device)
+    pid, nprocs = ClimaComms.init(context)
+    iamroot = ClimaComms.iamroot(context)
+    if iamroot
+        println("running test on $device device with $nprocs processes")
+    end
+
+    FT = Float64
+    radius = FT(128)
+    zlim = (0, 1)
+    helem = 4
+    Nq = 4
+    zelem = 10
+    vertdomain = Domains.IntervalDomain(
+        Geometry.ZPoint{FT}(zlim[1]),
+        Geometry.ZPoint{FT}(zlim[2]);
+        boundary_tags = (:bottom, :top),
+    )
+    vertmesh = Meshes.IntervalMesh(vertdomain, nelems = zelem)
+    vtopology = Topologies.IntervalTopology(vcontext, vertmesh)
+    vspace = Spaces.CenterFiniteDifferenceSpace(vtopology)
+
+    hdomain = Domains.SphereDomain(radius)
+    hmesh = Meshes.EquiangularCubedSphere(hdomain, helem)
+    htopology = Topologies.Topology2D(context, hmesh)
+    quad = Spaces.Quadratures.GLL{Nq}()
+    hspace = Spaces.SpectralElementSpace2D(htopology, quad)
+    space = Spaces.ExtrudedFiniteDifferenceSpace(hspace, vspace)
+
+end
