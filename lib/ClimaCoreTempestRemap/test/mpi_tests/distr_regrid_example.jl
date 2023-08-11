aa082b5874e659917bd301eea7a8580955a205cc
Julia Sloan <jsloan@caltech.edu>
2023-08-11 13:43:38 -0700

    working on example

--- lib/ClimaCoreTempestRemap/test/mpi_tests/distr_regrid_example.jl
+++ lib/ClimaCoreTempestRemap/test/mpi_tests/distr_regrid_example.jl
@@ -39,7 +39,6 @@ else
     rank = MPI.Comm_rank(comm)
 end
 root_pid = 0
-@show pid, nprocs
 
 # construct domain
 domain = Domains.RectangleDomain(
@@ -66,12 +65,7 @@ target_space_serial = make_space(domain, target_nq, target_nex, target_ney, comm
 
 # generate source data on source space
 source_data = Fields.ones(source_space)
-source_array = dropdims(parent(source_data), dims = 3)
-source_2d = reshape(
-    source_array,
-    (size(source_array)[1], size(source_array)[2] * size(source_array)[3]),
-)
-Nf = size(source_array, 3) # Nf is number of fields being remapped
+source_data_serial = Fields.ones(source_space_serial)
 
 
 # generate weights (no remapping in x direction, so we really only need y_weights)
@@ -102,9 +96,11 @@ end
 #     source_ind_to_pid[source_linear_inds[n]] = source_space.topology.elempid[elem]
 # end
 
+
 # we need to use the serial space here to maintain information about which pid each elem is on
 # if we use elempid on the distributed space, elem is in range [1...m_pid] for each pid,
 #  this results in elempid of 1 for all elems since counting restarts
+# we need target_ind_to_pid instead of elempid to get nodal gidx -> pid
 if ClimaComms.iamroot(comms_ctx)
     target_global_inds = collect(Spaces.all_nodes(target_space_serial))
     target_ind_to_pid = Dict{Int, Int}()
@@ -133,65 +129,71 @@ target_ind_to_pid = ClimaComms.bcast(comms_ctx, target_ind_to_pid)
 # target_ind_to_pid = Dict(zip(rows, pids))
 # @show target_ind_to_pid
 
-# create map from global to local source indices
-source_global_inds = collect(Spaces.all_nodes(source_space_serial))
-source_local_inds = collect(Spaces.all_nodes(source_space))
-@show source_local_inds
-n_source_local_inds = length(source_local_inds)
 
-# construct graph context to exchange information
-send_buf = [n_source_local_inds]
-# send_pids = collect(1:nprocs)
-send_pids = findall(p -> p != pid, collect(1:nprocs))
+# # create map from global to local source indices
+# # START source index map
+# source_global_inds = LinearIndices(collect(Spaces.all_nodes(source_space_serial)))
+# source_local_inds = LinearIndices(collect(Spaces.all_nodes(source_space)))
+
+# n_source_local_inds = length(source_local_inds)
+
+# # TODO take global, local indices and construct map, then fill send_buf with this map
+
+# # construct graph context to exchange information
+# send_buf = [n_source_local_inds]
+# send_pids = findall(p -> p != pid, collect(1:nprocs))
 # send_lengths = Int.(ones(length(send_pids)))
-# we receive one value from each process
-recv_buf = FT.(zeros(nprocs))
-recv_lengths = Int.(ones(length(send_pids)))
-recv_pids = send_pids
-# TODO do I need to append my value to recv_buf?
+# # this exchanges the number of local indices on each process
+# # each pid then needs to use this to index from global to local
+# recv_buf = FT.(zeros(nprocs))
+# recv_lengths = Int.(ones(length(send_pids)))
+# recv_pids = send_pids
+# # TODO do I need to append my value to recv_buf? how do I know where?
 
-gc_source_inds = ClimaComms.graph_context(
-    comms_ctx,
-    send_buf,
-    send_lengths,
-    send_pids,
-    recv_buf,
-    recv_lengths,
-    recv_pids,
-)
-@show send_buf
-# exchange information between processes (like `allgather`)
-ClimaComms.start(gc_source_inds)
-ClimaComms.finish(gc_source_inds)
-@show recv_buf
+# gc_source_inds = ClimaComms.graph_context(
+#     comms_ctx,
+#     send_buf,
+#     send_lengths,
+#     send_pids,
+#     recv_buf,
+#     recv_lengths,
+#     recv_pids,
+# )
+# @show send_buf
+# @show recv_buf
+# # exchange information between processes (like `allgather`)
+# ClimaComms.start(gc_source_inds)
+# ClimaComms.finish(gc_source_inds)
+# @show recv_buf
 
-pid_n_source_inds = recv_buf
-@show pid_n_source_inds
+# pid_n_source_inds = recv_buf
+# @show pid_n_source_inds
 
-# count all source indices in processes before this one
-prev_source_inds = 0
-for p in 1:pid - 1
-    global prev_source_inds += pid_n_source_inds[p]
-end
+# # count all source indices in processes before this one
+# prev_source_inds = 0
+# for p in 1:pid - 1
+#     global prev_source_inds += pid_n_source_inds[p]
+# end
 
-# map global to local source indices
-source_global_to_local = Dict{typeof(pid), FT}()
-for i in 1:n_source_local_inds
-    source_global_to_local[prev_source_inds + i] = source_local_inds[i]
-end
-@show source_global_to_local
-
+# # map global to local source indices
+# source_global_to_local = Dict{typeof(pid), FT}()
+# for i in 1:length(source_global_inds)
+#     source_global_to_local[prev_source_inds + i] = source_local_inds[i]
+# end
+# @show source_global_to_local
+# # END source index map
 
 
 # loop over rows in weight matrix, compute dot prod for each row
 # TODO convert these to sparse representations later on
 send_row_sums = FT.(zeros(weights_serial.m))
 
-local_rows = findall(j -> target_ind_to_pid[j] == pid, collect(1:(weights_serial.m)))
+# target_ind_to_pid maps nodal index to pid, where elempid maps element index to pid
+local_rows = filter(j -> target_ind_to_pid[j] == pid, 1:(weights_serial.m))
 # local_rows = collect(keys(target_ind_to_pid))
 n_local_rows = length(local_rows)
-local_row_sums = FT.(zeros(n_local_rows))
-send_counts = Dict{typeof(pid), FT}()
+local_row_sums = FT.(zeros(weights_serial.m))
+send_counts = zeros(Int, nprocs)
 # counts = FT.(zeros(nprocs))
 @show local_rows
 
@@ -200,24 +202,22 @@ for j in 1:(weights_serial.m)
     target_pid = target_ind_to_pid[j]
     # accumulate number of non-local rows (collecting to send)
     if target_pid != pid
-        if haskey(send_counts, target_pid)
-            send_counts[target_pid] += 1
-        else
-            send_counts[target_pid] = 1
-        end
+        send_counts[target_pid] += 1
     end
     # loop over indices of weights in this row
     for n in findall(x -> x == j, weights_serial.rowval)
-        @show n
         # n lets us index into weights.nzval
         wt = weights_serial.nzval[n]
 
         # find local column (source) index of this weight
         # index 2 gives weights.colptr
         # ISSUE: we need to use weights_serial, but this gives the global source index, not local
+        #  and we need to index into source_data using local index since it's generated on the distributed space
+
+        # TODO use distributed source data
         #  we either need a global to local source index map, or a way to index into serial source data
-        source_ind_global = SparseArrays.findnz(weights_serial)[2][n]
-        source_val = vec(parent(source_data))[source_ind]
+        source_gidx = SparseArrays.findnz(weights_serial)[2][n]
+        source_val = vec(parent(source_data_serial))[source_gidx]
 
         # add to local array if target index is local to this process
         if target_pid == pid
@@ -232,25 +232,30 @@ end
 @show send_row_sums
 
 # set up send buffer for dot products - gather dot product data and length for each process
-# counts[i] gives the number of row (target) inds process i is responsible for
+# send_counts[i] gives the number of row (target) inds process i is responsible for
 #  (i.e. length of send buffer it will receive from each other process)
-# TODO does this have to be on root?
-# counts = [count(==(i), target_ind_to_pid) for i in unique(target_ind_to_pid)]
 # Note: send_counts will be empty for non-distributed runs
-if !isempty(send_counts)
-    @assert send_counts[pid] == target_ney * target_nq^2 # TODO is this right
-end
-
 
 # set up graph_context for information exchange
 #  - see graph_context code here https://github.com/CliMA/ClimaComms.jl/blob/main/src/mpi.jl#L120
 send_array = send_row_sums
-send_lengths = send_counts
-send_pids = findall(p -> p != pid, collect(1:nprocs))
+send_pids = filter(p -> send_counts[p] > 0, 1:nprocs)
+send_lengths = send_counts[send_pids]
+@assert length(send_lengths) == nprocs - 1
+
+
 recv_array = FT.(zeros(weights_serial.m))
 # from each process, we receive number of rows this process responsible for
-recv_lengths = FT.(n_local_rows * ones(length(send_pids)))
+recv_lengths = Int.(n_local_rows * ones(length(send_pids)))
 recv_pids = send_pids
+
+@show comms_ctx
+@show send_array
+@show send_lengths
+@show send_pids
+@show recv_array
+@show recv_lengths
+@show recv_pids
 
 gc_row_sums = ClimaComms.graph_context(
     comms_ctx,
@@ -266,20 +271,38 @@ gc_row_sums = ClimaComms.graph_context(
 ClimaComms.start(gc_row_sums)
 ClimaComms.finish(gc_row_sums)
 
+# TODO one pid has recv_array all 0 after exchange
+@show local_row_sums
+@show recv_array
 
-# combine received data with local data
-# loop over received rows, add received sums to local sum for each row
-for j in local_rows
-    # TODO how to index into recv_array correctly? - this assumes it's ordered
-    # `recv_array` contains multiplied `weight*source_data` vals, so just add to local sum for dot product
-    # `n * n_local_rows` factor loops over received data from all other processes, skipping local rows
-    for n in collect(0:length(recv_pids))
-        local_row_sums[j] += recv_array[j + n * n_local_rows]
+# # combine received data with local data
+# # loop over received rows, add received sums to local sum for each row
+# for j in local_rows
+#     # TODO how to index into recv_array correctly? - this assumes it's ordered
+#     # `recv_array` contains multiplied `weight*source_data` vals, so just add to local sum for dot product
+#     # `n * n_local_rows` factor loops over received data from all other processes, skipping local rows
+#     for n in collect(0:length(recv_pids))
+#         @show j
+#         @show n
+#         @show j + n * n_local_rows
+#         local_row_sums[j] += recv_array[j + n * n_local_rows]
+#     end
+# end
+
+# loop over all rows (target indices) of weight matrix
+for j in 1:(weights_serial.m)
+    target_pid = target_ind_to_pid[j]
+
+    # if row is non-local on target, add received value to row sums
+    if !(target_pid == pid)
+        local_row_sums[j] += recv_array[j]
     end
 end
+
 
 @show local_row_sums
 
 # TODO the length of local_row_sums = number of redundant nodes we have
 #  but still need to convert this vector to CC convention
-# return local_row_sums
+remapped_field = Fields.ones(target_space)
+remapped_field .= local_row_sums
