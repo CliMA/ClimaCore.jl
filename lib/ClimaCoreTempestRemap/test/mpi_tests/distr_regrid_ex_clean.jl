5e483d2ad68b4f255ba79f59bac21ca3e84d130f
Julia Sloan <jsloan@caltech.edu>
2023-08-11 18:03:43 -0700

    weight scatter impl, needs debug

--- lib/ClimaCoreTempestRemap/test/mpi_tests/distr_regrid_ex_clean.jl
+++ lib/ClimaCoreTempestRemap/test/mpi_tests/distr_regrid_ex_clean.jl
@@ -58,6 +58,147 @@ function gen_weights(
     weights = Operators.overlap(target_space_serial, source_space_serial)
 end
 
+# TODO combine this with col_offsets function?
+# Calculate the number of nonzero values (weights) on each pid
+function calc_counts(weights, source_space, nprocs)
+    # create map from source linear ind to pid
+    s_inds = collect(Spaces.all_nodes(source_space))
+    s_lind_to_pid = zeros(Int, weights.n)
+
+    # extract element number for each linear index, use this to get pid
+    for (ind_lin, ind_ije) in enumerate(s_inds)
+        elem = ind_ije[2]
+        pid = source_space.topology.elempid[elem]
+        s_lind_to_pid[ind_lin] = pid
+    end
+
+    # get number of nonzero weights on each process
+    nzval_counts = zeros(Int, nprocs)
+    for ind_lin in LinearIndices(s_inds)
+        pid = s_lind_to_pid[ind_lin]
+        nzval_counts[pid] += weights.colptr[ind_lin + 1] - weights.colptr[ind_lin]
+    end
+
+    return nzval_counts
+end
+
+# Calculate the number of columns containing weights on each process
+function calc_col_offsets(weights, source_space, nprocs)
+    s_inds = collect(Spaces.all_nodes(source_space))
+    # create map from source column ind to pid
+    s_col_to_pid = zeros(Int, weights.n)
+
+    # extract element number for each linear index, use this to get pid
+    for (ind_lin, ind_ije) in enumerate(s_inds)
+        col = ind_ije[1][2]
+
+        # if col is unseen, continue
+        if s_col_to_pid[col] == 0
+            elem = ind_ije[2]
+            pid = source_space.topology.elempid[elem]
+
+            s_col_to_pid[col] = pid
+        end
+    end
+
+    # get number of columns on each process
+    num_cols = zeros(Int, nprocs)
+    for p in 1:nprocs
+        num_cols[p] = count(c -> c == p, s_col_to_pid)
+    end
+
+    # go through columns and accumulate offset for each pid
+    col_offsets = zeros(Int, nprocs)
+    offset = Int(1)
+    for p in 1:nprocs
+        col_offsets[p] = offset
+        offset += num_cols[p]
+    end
+
+    return col_offsets
+end
+
+# Scatter data where each process receives one value
+#  Use for counts and column offest
+function scatter_length1(data, comms_ctx, inplace::Bool)
+    # if ClimaComms.iamroot(comms_ctx)
+    #     # set up buffer to send 1 value to each pid
+    #     sendbuf = MPI.UBuffer(data, Int(1))
+    #     # if inplace, don't modify root recvbuf
+    #     #  when exchanging counts, want root to retain info about all processes
+    #     recvbuf = inplace ? MPI.IN_PLACE : MPI.Buffer(zeros(Int, 1))
+    # else
+    #     # send nothing on non-root processes
+    #     sendbuf = nothing
+    #     # create receive buffer of length 1 on each process
+    #     recvbuf = MPI.Buffer(zeros(Int, 1))
+    # end
+
+    # # scatter data to all processes
+    # MPI.Scatter!(sendbuf, recvbuf, comms_ctx.mpicomm)
+    if ClimaComms.iamroot(comms_ctx)
+        # set up buffer to send 1 value to each pid
+        sendbuf = MPI.UBuffer(data, Int(1))
+        MPI.Scatter!(sendbuf, MPI.IN_PLACE, comms_ctx.mpicomm)
+        return data
+    else
+        recvbuf = MPI.Buffer(zeros(Int, 1))
+        MPI.Scatter!(nothing, recvbuf, comms_ctx.mpicomm)
+        return recvbuf.data
+    end
+
+    # return recvbuf.data
+end
+
+# Scatter data where each process receives multiple values
+function scatterv_exchange(data, comms_ctx, send_counts, recv_length)
+    if ClimaComms.iamroot(comms_ctx)
+        # set up buffer to send `length` values to each pid
+        sendbuf = MPI.VBuffer(data, send_counts)
+    else
+        # send nothing on non-root processes
+        sendbuf = nothing
+    end
+    # create receive buffer of specified length on each process
+    recvbuf = MPI.Buffer(zeros(Int, recv_length))
+
+    # scatter data to all processes
+    MPI.Scatterv!(sendbuf, recvbuf, comms_ctx.mpicomm)
+    return recvbuf.data
+end
+
+# Take weight matrix mapping between serial spaces, distribute among processes
+function distr_weights(weights, source_space, target_space, comms_ctx, nprocs)
+    comm = comms_ctx.mpicomm
+    # calculate number of nonzero vals (weights) on each pid to fill counts
+    if ClimaComms.iamroot(comms_ctx)
+        counts = calc_counts(weights, source_space, nprocs)
+        col_offsets = calc_col_offsets(weights, source_space, nprocs)
+        nzvals = weights.nzval
+        row_vals = weights.rowval
+    else
+        counts = nothing
+        col_offsets = nothing
+        nzvals = nothing
+        row_vals = nothing
+    end
+    # scatter to each process (counts will be used to create receive buffer lengths)
+    #  do this in place so root retains info about all pids' counts
+    n_weights_mypid = scatter_length1(counts, comms_ctx, true)[1]
+    col_offset = scatter_length1(col_offsets, comms_ctx, false)[1]
+
+    # scatter weights
+    weights = scatterv_exchange(nzvals, comms_ctx, counts, n_weights_mypid)
+
+    # scatter row indices
+    row_inds = scatterv_exchange(row_vals, comms_ctx, counts, n_weights_mypid)1
+
+    @show weights
+    @show row_inds
+    @show col_offsets
+    return weights, row_inds, col_offsets
+end
+
 
 # set up MPI info
 comms_ctx = ClimaComms.MPICommsContext()
@@ -89,11 +230,13 @@ target_space = make_space(domain, target_nq, target_nex, target_ney, comms_ctx)
 # generate weights matrix on root process
 if ClimaComms.iamroot(comms_ctx)
     weights = gen_weights(source_space, target_space)
+else
+    weights = nothing
 end
 
+# distribute (scatter) weights to all processes
+weights, row_inds, col_offsets = distr_weights(weights, source_space, target_space, comms_ctx, nprocs)
 
-
-
-
-# generate source data on distributed source space
-source_data = Fields.ones(source_space)
+@show weights
+@show row_inds
+@show col_offsets
