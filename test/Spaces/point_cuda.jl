6719dfa92f59ef0823b9e3a1a4e08dd9c5668d26
Simon Byrne <simonbyrne@gmail.com>
2023-11-17 13:18:14 -0800

    define Adapt for PointSpace

---
+++ test/Spaces/point_cuda.jl
@@ -0,0 +1,23 @@
+import ClimaCore
+import ClimaCore: Domains, Topologies, Meshes, Spaces, Geometry, column, Fields
+import ClimaComms
+using Test
+
+compare(cpu, gpu) = all(parent(cpu) .≈ Array(parent(gpu)))
+compare(cpu, gpu, sym) =
+    all(parent(getproperty(cpu, sym)) .≈ Array(parent(getproperty(gpu, sym))))
+
+@testset "CuArray-backed point spaces" begin
+    cpu_context =
+        ClimaComms.SingletonCommsContext(ClimaComms.CPUSingleThreaded())
+    gpu_context = ClimaComms.SingletonCommsContext(ClimaComms.CUDADevice())
+
+    point = Geometry.ZPoint(1.0)
+
+    cpuspace = Spaces.PointSpace(cpu_context, point)
+    gpuspace = Spaces.PointSpace(gpu_context, point)
+
+    # Test that all geometries match with CPU version:
+    @test compare(cpuspace, gpuspace, :local_geometry)
+
+end
