3c22fae9344832f20d12671ba5f4cd0b398c8bdb
Simon Byrne <simonbyrne@gmail.com>
2024-01-17 15:37:15 -0800

    create function for fd geometry data

--- src/Grids/finitedifference.jl
+++ src/Grids/finitedifference.jl
@@ -49,28 +49,39 @@ end
 
 function _FiniteDifferenceGrid(topology::Topologies.IntervalTopology)
     global_geometry = Geometry.CartesianGlobalGeometry()
-    mesh = topology.mesh
-    CT = Meshes.coordinate_type(mesh)
-    AIdx = Geometry.coordinate_axis(CT)
-    # TODO: FD operators  hardcoded to work over the 3-axis, need to generalize
-    # similar to spectral operators
-    @assert AIdx == (3,) "FiniteDifference operations only work over the 3-axis (ZPoint) domain"
-    FT = eltype(CT)
     ArrayType = ClimaComms.array_type(topology)
     face_coordinates = collect(mesh.faces)
+    # construct on CPU, adapt to GPU
+    center_local_geometry, face_local_geometry = fd_geometry_data(
+        face_coordinates;
+        periodic = Topologies.isperiodic(topology),
+    )
+    return FiniteDifferenceGrid(
+        topology,
+        global_geometry,
+        Adapt.adapt(ArrayType, center_local_geometry),
+        Adapt.adapt(ArrayType, face_local_geometry),
+    )
+end
+
+function fd_geometry_data(
+    face_coordinates::AbstractVector{Geometry.ZPoint{FT}};
+    periodic,
+) where {FT}
+    AIdx == (3,)
+    CT = Geometry.ZPoint{FT}
     LG = Geometry.LocalGeometry{AIdx, CT, FT, SMatrix{1, 1, FT, 1}}
-    nface = length(face_coordinates) - Topologies.isperiodic(topology)
+
+    nface = length(face_coordinates) - periodic
     ncent = length(face_coordinates) - 1
-    # contstruct on CPU, copy to device at end
     center_local_geometry = DataLayouts.VF{LG}(Array{FT}, ncent)
     face_local_geometry = DataLayouts.VF{LG}(Array{FT}, nface)
+
     for i in 1:ncent
         # centers
         coord⁻ = Geometry.component(face_coordinates[i], 1)
         coord⁺ = Geometry.component(face_coordinates[i + 1], 1)
-        # at the moment we use a "discrete Jacobian"
-        # ideally we should use the continuous quantity via the derivative of the warp function
-        # could we just define this then as deriv on the mesh element coordinates?
+        # use a "discrete Jacobian"
         coord = (coord⁺ + coord⁻) / 2
         Δcoord = coord⁺ - coord⁻
         J = Δcoord
@@ -86,11 +97,12 @@ function _FiniteDifferenceGrid(topology::Topologies.IntervalTopology)
             ),
         )
     end
+
     for i in 1:nface
         coord = Geometry.component(face_coordinates[i], 1)
         if i == 1
             # bottom face
-            if Topologies.isperiodic(topology)
+            if periodic
                 Δcoord⁺ =
                     Geometry.component(face_coordinates[2], 1) -
                     Geometry.component(face_coordinates[1], 1)
@@ -104,7 +116,8 @@ function _FiniteDifferenceGrid(topology::Topologies.IntervalTopology)
                 J = coord⁺ - coord
                 WJ = J / 2
             end
-        elseif !Topologies.isperiodic(topology) && i == nface
+        elseif i == ncent + 1
+            @assert !periodic
             # top face
             coord⁻ = Geometry.component(face_coordinates[i - 1], 1)
             J = coord - coord⁻
@@ -116,7 +129,6 @@ function _FiniteDifferenceGrid(topology::Topologies.IntervalTopology)
             WJ = J
         end
         ∂x∂ξ = SMatrix{1, 1}(J)
-        ∂ξ∂x = SMatrix{1, 1}(inv(J))
         face_local_geometry[i] = Geometry.LocalGeometry(
             CT(coord),
             J,
@@ -127,12 +139,7 @@ function _FiniteDifferenceGrid(topology::Topologies.IntervalTopology)
             ),
         )
     end
-    return FiniteDifferenceGrid(
-        topology,
-        global_geometry,
-        Adapt.adapt(ArrayType, center_local_geometry),
-        Adapt.adapt(ArrayType, face_local_geometry),
-    )
+    return (center_local_geometry, face_local_geometry)
 end
 
 
