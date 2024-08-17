5cd7a7123ae7249826f98b29dcca8ef3c07393b2
Charles Kawczynski <kawczynski.charles@gmail.com>
2024-08-17 11:15:45 -0400

    Improve inference in grid constructors

--- test/Spaces/opt_spaces.jl
+++ test/Spaces/opt_spaces.jl
@@ -1,9 +1,10 @@
 #=
 julia --project=.buildkite
+ENV["CLIMACOMMS_DEVICE"] = "CUDA";
 using Revise; include(joinpath("test", "Spaces", "opt_spaces.jl"))
 =#
 import ClimaCore
-import ClimaCore: Spaces, Grids
+import ClimaCore: Spaces, Grids, Topologies
 using Test
 include(
     joinpath(pkgdir(ClimaCore), "test", "TestUtilities", "TestUtilities.jl"),
@@ -34,19 +35,19 @@ end
 #! format: off
     if ClimaComms.device(context) isa ClimaComms.CUDADevice
         test_n_failures(86,   TU.PointSpace, context)
-        test_n_failures(144,  TU.SpectralElementSpace1D, context)
+        test_n_failures(141,  TU.SpectralElementSpace1D, context)
         test_n_failures(1141, TU.SpectralElementSpace2D, context)
-        test_n_failures(123,  TU.ColumnCenterFiniteDifferenceSpace, context)
-        test_n_failures(123,  TU.ColumnFaceFiniteDifferenceSpace, context)
-        test_n_failures(1131, TU.SphereSpectralElementSpace, context)
-        test_n_failures(1139, TU.CenterExtrudedFiniteDifferenceSpace, context)
-        test_n_failures(1139, TU.FaceExtrudedFiniteDifferenceSpace, context)
+        test_n_failures(3,  TU.ColumnCenterFiniteDifferenceSpace, context)
+        test_n_failures(4,  TU.ColumnFaceFiniteDifferenceSpace, context)
+        test_n_failures(1147, TU.SphereSpectralElementSpace, context)
+        test_n_failures(1146, TU.CenterExtrudedFiniteDifferenceSpace, context)
+        test_n_failures(1146, TU.FaceExtrudedFiniteDifferenceSpace, context)
     else
         test_n_failures(0,    TU.PointSpace, context)
         test_n_failures(137,  TU.SpectralElementSpace1D, context)
         test_n_failures(310,  TU.SpectralElementSpace2D, context)
-        test_n_failures(118,  TU.ColumnCenterFiniteDifferenceSpace, context)
-        test_n_failures(118,  TU.ColumnFaceFiniteDifferenceSpace, context)
+        test_n_failures(4,  TU.ColumnCenterFiniteDifferenceSpace, context)
+        test_n_failures(5,  TU.ColumnFaceFiniteDifferenceSpace, context)
         test_n_failures(316,  TU.SphereSpectralElementSpace, context)
         test_n_failures(321,  TU.CenterExtrudedFiniteDifferenceSpace, context)
         test_n_failures(321,  TU.FaceExtrudedFiniteDifferenceSpace, context)
@@ -56,11 +57,10 @@ end
         # separately:
 
         space = TU.CenterExtrudedFiniteDifferenceSpace(Float32; context=ClimaComms.context())
-        # @test_opt Grids._SpectralElementGrid2D(Spaces.topology(space), Spaces.quadrature_style(space); enable_bubble=false)
-
-        result = JET.@report_opt Grids._SpectralElementGrid2D(Spaces.topology(space), Spaces.quadrature_style(space); enable_bubble=false)
+        Nh = Val(Topologies.nlocalelems(Spaces.topology(space)))
+        result = JET.@report_opt Grids._SpectralElementGrid2D(Spaces.topology(space), Spaces.quadrature_style(space), Val(Nh); enable_bubble=false)
         n_found = length(JET.get_reports(result.analyzer, result.result))
-        n_allowed = 187
+        n_allowed = 0
         @test n_found ≤ n_allowed
         if n_found < n_allowed
             @info "Inference may have improved for _SpectralElementGrid2D: (n_found, n_allowed) = ($n_found, $n_allowed)"
