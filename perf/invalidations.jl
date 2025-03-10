14d0bae1595e806625a3678dde9e62e744e2b335
Charles Kawczynski <kawczynski.charles@gmail.com>
2025-03-10 09:11:25 -0400

    Update SnoopCompile + compat and fixes (#2224)

--- perf/invalidations.jl
+++ perf/invalidations.jl
@@ -1,8 +1,8 @@
-# From: https://timholy.github.io/SnoopCompile.jl/stable/snoopr/
+# From: https://timholy.github.io/SnoopCompile.jl/dev/tutorials/invalidations/#Tutorial-on-@snoop_invalidations
 ENV["TEST_NAME"] = "sphere/held_suarez_rhoe"
 ENV["FLOAT_TYPE"] = "Float64"
 using SnoopCompileCore
-invalidations = @snoopr begin
+invalidations = @snoop_invalidations begin
     include(joinpath(dirname(@__DIR__), "examples", "hybrid", "driver.jl"))
     nothing
 end;
