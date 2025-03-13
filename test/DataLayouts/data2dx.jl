6095961152e544c28d30cfd34d1bb3362fda7709
sriharshakandala <sriharsha.kvs@gmail.com>
2025-03-13 16:34:35 -0700

    Add `array2data` and `data2array` functionality with transpose option for RRTMGPInterface.

--- test/DataLayouts/data2dx.jl
+++ test/DataLayouts/data2dx.jl
@@ -5,7 +5,16 @@ using Revise; include(joinpath("test", "DataLayouts", "data2dx.jl"))
 using Test
 using ClimaComms
 using ClimaCore.DataLayouts
-import ClimaCore.DataLayouts: VF, IJFH, VIJFH, slab, column, slab_index, vindex
+import ClimaCore.DataLayouts:
+    VF,
+    IJFH,
+    VIJFH,
+    slab,
+    column,
+    slab_index,
+    vindex,
+    data2array_rrtmgp!,
+    array2data_rrtmgp!
 
 device = ClimaComms.device()
 ArrayType = ClimaComms.array_type(device)
@@ -126,4 +135,43 @@ end
     @test parent(data_vijfh .+ data_ijfh) == FT[
         f == 1 ? 3 : 1 for v in 1:3, i in 1:2, j in 1:2, f in 1:2, h in 1:2
     ]
+end
+
+@testset "VIJFH data2array_rrtmgp!" begin
+    Nv = 10 # number of vertical levels
+    Nij = 4 # Nij × Nij nodal points per element
+    Nh = 10 # number of elements
+
+    nl = Nv # number of levels/layers in the array
+    ncol = Nij * Nij * Nh # number of columns in the array
+
+    for FT in (Float32, Float64)
+        data1 = VIJFH{FT, Nv, Nij}(
+            ArrayType(
+                reshape(FT(1.0):(Nv * Nij * Nij * Nh), Nv, Nij, Nij, 1, Nh),
+            ),
+        )
+
+        array1 = ArrayType{FT}(undef, nl, ncol)
+        array1t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array1, data1, Val(false))
+        data2array_rrtmgp!(array1t, data1, Val(true))
+
+        @test array1 == ArrayType(transpose(array1t))
+
+        array2 = ArrayType(reshape(FT(1.0):(Nv * Nij * Nij * Nh), Nv, :))
+        array2t = ArrayType{FT}(undef, ncol, nl)
+
+        data2 = VIJFH{FT, Nv, Nij}(ArrayType{FT}(undef, Nv, Nij, Nij, 1, Nh))
+
+        array2data_rrtmgp!(data2, array2, Val(false))
+        @test parent(data2) == parent(data1)
+
+        array2t = ArrayType(transpose(Array(array2)))
+
+        parent(data2) .= NaN
+        array2data_rrtmgp!(data2, array2t, Val(true))
+        @test parent(data2) == parent(data1)
+    end
 end
