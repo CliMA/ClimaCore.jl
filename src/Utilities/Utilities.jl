575f0679b700336cc44015629f67028b1a1b6a70
Gabriele Bozzola <gbozzola@caltech.edu>
2025-03-13 11:32:34 -0700

    Move unionall_type to Utilities, add docs (#2238)
    This PR moves the two identical instances of `unionall_type` (and the
    similarly named `union_all_type`) to a common function in `Utilities`.
    It also adds examples to clarify what the function does.

--- src/Utilities/Utilities.jl
+++ src/Utilities/Utilities.jl
@@ -30,4 +30,35 @@ Base.@propagate_inbounds linear_ind(n::NTuple, ci::CartesianIndex) =
 Base.@propagate_inbounds linear_ind(n::NTuple, loc::NTuple) =
     linear_ind(n, CartesianIndex(loc))
 
+"""
+    unionall_type(::Type{T})
+
+Extract the type of the input, and strip it of any type parameters.
+
+This is useful when one needs the generic constructor for a given type.
+
+Example
+=======
+```julia
+julia> unionall_type(typeof([1, 2, 3]))
+Array
+
+julia> struct Foo{A, B}
+               a::A
+               b::B
+       end
+
+julia> unionall_type(typeof(Foo(1,2)))
+Foo
+```
+"""
+function unionall_type(::Type{T}) where {T}
+    # NOTE: As of version 1.12, there is no simple, user-friendly way to extract
+    # the generic type of T, so we need to reach for the internals in Julia.
+    # Hopefully, Julia will introduce a simpler, more stable way to do this in a
+    # future release.
+    return T.name.wrapper
+end
+
+
 end # module
