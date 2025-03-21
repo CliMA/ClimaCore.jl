ee3a3c2de80f6c482f93d694cbdc6b6deb71f245
sriharshakandala <sriharsha.kvs@gmail.com>
2025-03-21 15:27:21 -0700

    Work on parent array.

--- ext/cuda/data_layouts_rrtmgp.jl
+++ ext/cuda/data_layouts_rrtmgp.jl
@@ -2,9 +2,9 @@ import ClimaCore.DataLayouts: data2array_rrtmgp!, array2data_rrtmgp!
 
 function data2array_rrtmgp!(
     array::CUDA.CuArray,
-    data::AbstractData,
+    data::D,
     ::Val{trans},
-) where {trans}
+) where {trans, D <: Union{VF, VIFH, VIHF, VIJFH, VIJHF}}
     (nl, ncol) = trans ? size(array) : reverse(size(array))
     Ni, Nj, Nk, Nv, Nh = Base.size(data)
     @assert nl * ncol == Ni * Nj * Nk * Nv * Nh
@@ -33,77 +33,106 @@ function data2array_rrtmgp_kernel!(
     Ni, Nj, Nk, Nv, Nh = Base.size(data)
     ncol = Ni * Nj * Nk * Nh
     # obtain the column number processed by each thread
-    gid = threadIdx().x + (blockIdx().x - 1) * blockDim().x
-    if gid ≤ ncol
-        h = cld(gid, Ni * Nj * Nk)
-        idx = gid - (h - 1) * Ni * Nj * Nk
+    gidx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    if gidx ≤ ncol
+        h = cld(gidx, Ni * Nj * Nk)
+        idx = gidx - (h - 1) * Ni * Nj * Nk
         k = cld(idx, Ni * Nj)
         idx = idx - (k - 1) * Ni * Nj
         j = cld(idx, Ni)
         i = idx - (j - 1) * Ni
         @inbounds begin
             for v in 1:Nv
-                colidx =
-                    i +
-                    (j - 1) * Ni +
-                    (k - 1) * Ni * Nj +
-                    (h - 1) * Ni * Nj * Nk
                 cidx = CartesianIndex(i, j, k, v, h)
-                trans ? (array[colidx, v] = data[cidx]) :
-                (array[v, colidx] = data[cidx])
+                trans ? (array[gidx, v] = data[cidx][1]) :
+                (array[v, gidx] = data[cidx][1])
             end
         end
     end
     return nothing
 end
 
+_get_kernel_function(::VIJFH) = array2data_rrtmgp_VIJFH_kernel!
+_get_kernel_function(::VIFH) = array2data_rrtmgp_VIFH_kernel!
+_get_kernel_function(::VF) = array2data_rrtmgp_VF_kernel!
+
 function array2data_rrtmgp!(
-    data::AbstractData,
+    data::D,
     array::CUDA.CuArray,
     ::Val{trans},
-) where {trans}
+) where {trans, D <: Union{VF, VIFH, VIHF, VIJFH, VIJHF}}
     (nl, ncol) = trans ? size(array) : reverse(size(array))
-    Ni, Nj, Nk, Nv, Nh = Base.size(data)
-    @assert nl * ncol == Ni * Nj * Nk * Nv * Nh
-    @assert prod(size(parent(data))) == Ni * Nj * Nk * Nv * Nh # verify Nf == 1
-    kernel = CUDA.@cuda launch = false array2data_rrtmgp_kernel!(
-        data,
-        array,
-        Val(trans),
-    )
+    Ni, Nj, _, Nv, Nh = Base.size(data)
+    @assert nl * ncol == Ni * Nj * Nv * Nh
+    @assert prod(size(parent(data))) == Ni * Nj * Nv * Nh # verify Nf == 1
+
+    kernelfun! = _get_kernel_function(data)
+
+    kernel =
+        CUDA.@cuda launch = false kernelfun!(parent(data), array, Val(trans))
     kernel_config = CUDA.launch_configuration(kernel.fun)
-    nitems = Ni * Nj * Nk * Nh
+    nitems = Ni * Nj * Nh
     nthreads, nblocks = linear_partition(nitems, kernel_config.threads)
-    CUDA.@cuda threads = nthreads blocks = nblocks array2data_rrtmgp_kernel!(
-        data,
+    CUDA.@cuda threads = nthreads blocks = nblocks kernelfun!(
+        parent(data),
         array,
         Val(trans),
     )
     return nothing
 end
 
-function array2data_rrtmgp_kernel!(
-    data::AbstractData,
+function array2data_rrtmgp_VIJFH_kernel!(
+    parentdata::AbstractArray,
     array::AbstractArray,
     ::Val{trans},
 ) where {trans}
-    Ni, Nj, Nk, Nv, Nh = Base.size(data)
-    ncol = Ni * Nj * Nk * Nh
+    Nv, Ni, Nj, _, Nh = size(parentdata)
+    ncol = Ni * Nj * Nh
     # obtain the column number processed by each thread
-    gid = threadIdx().x + (blockIdx().x - 1) * blockDim().x
-    if gid ≤ ncol
-        h = cld(gid, Ni * Nj * Nk)
-        idx = gid - (h - 1) * Ni * Nj * Nk
-        k = cld(idx, Ni * Nj)
-        idx = idx - (k - 1) * Ni * Nj
-        j = cld(idx, Ni)
-        i = idx - (j - 1) * Ni
-        colidx = i + (j - 1) * Ni + (k - 1) * Ni * Nj + (h - 1) * Ni * Nj * Nk
-        @inbounds begin
-            for v in 1:Nv
-                data[CartesianIndex(i, j, k, v, h)] =
-                    trans ? array[colidx, v] : array[v, colidx]
-            end
+    gidx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    if gidx ≤ ncol
+        h = cld(gidx, Ni * Nj)
+        j = cld(gidx - (h - 1) * Ni * Nj, Ni)
+        i = gidx - (h - 1) * Ni * Nj - (j - 1) * Ni
+        for v in 1:Nv
+            @inbounds parentdata[v, i, j, 1, h] =
+                trans ? array[gidx, v] : array[v, gidx]
+        end
+    end
+    return nothing
+end
+
+function array2data_rrtmgp_VIFH_kernel!(
+    parentdata::AbstractArray,
+    array::AbstractArray,
+    ::Val{trans},
+) where {trans}
+    Nv, Ni, _, Nh = size(parentdata)
+    ncol = Ni * Nh
+    # obtain the column number processed by each thread
+    gidx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    if gidx ≤ ncol
+        h = cld(gidx, Ni)
+        i = gidx - (h - 1) * Ni
+        for v in 1:Nv
+            @inbounds parentdata[v, i, 1, h] =
+                trans ? array[gidx, v] : array[v, gidx]
+        end
+    end
+    return nothing
+end
+
+function array2data_rrtmgp_VF_kernel!(
+    parentdata::AbstractArray,
+    array::AbstractArray,
+    ::Val{trans},
+) where {trans}
+    Nv, _ = size(parentdata)
+    # obtain the column number processed by each thread
+    gidx = threadIdx().x + (blockIdx().x - 1) * blockDim().x
+    if gidx ≤ 1
+        for v in 1:Nv
+            @inbounds parentdata[v, 1] = trans ? array[gidx, v] : array[v, gidx]
         end
     end
     return nothing
--- test/DataLayouts/datarrtmgp.jl
+++ test/DataLayouts/datarrtmgp.jl
@@ -6,10 +6,12 @@ using Test
 using ClimaComms
 ClimaComms.@import_required_backends
 using ClimaCore.DataLayouts
+using ClimaCore.Geometry
 import ClimaCore.DataLayouts:
     VF,
-    IJFH,
+    VIFH,
     VIJFH,
+    VIJHF,
     slab,
     column,
     slab_index,
@@ -56,5 +58,135 @@ ArrayType = ClimaComms.array_type(device)
         parent(data2) .= NaN
         array2data_rrtmgp!(data2, array2t, Val(true))
         @test parent(data2) == parent(data1)
+
+        data3 = VIJFH{WVector{FT}, Nv, Nij}(
+            ArrayType(
+                reshape(FT(1.0):(Nv * Nij * Nij * Nh), Nv, Nij, Nij, 1, Nh),
+            ),
+        )
+        array3 = ArrayType{FT}(undef, nl, ncol)
+        array3t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array3, data3, Val(false))
+        data2array_rrtmgp!(array3t, data3, Val(true))
+
+        @test array3 == ArrayType(transpose(array3t))
+
+
+        array4 = ArrayType(reshape(FT(1.0):(Nv * Nij * Nij * Nh), Nv, :))
+        array4t = ArrayType{FT}(undef, ncol, nl)
+
+        data4 = VIJFH{FT, Nv, Nij}(ArrayType{FT}(undef, Nv, Nij, Nij, 1, Nh))
+
+        array2data_rrtmgp!(data4, array4, Val(false))
+        @test parent(data4) == parent(data4)
+    end
+end
+
+@testset "VIFH data2array_rrtmgp!" begin
+    Nv = 10 # number of vertical levels
+    Ni = 4 # Nij × Nij nodal points per element
+    Nh = 10 # number of elements
+
+    nl = Nv # number of levels/layers in the array
+    ncol = Ni * Nh # number of columns in the array
+
+    for FT in (Float32, Float64)
+        data1 = VIFH{FT, Nv, Ni}(
+            ArrayType(reshape(FT(1.0):(Nv * Ni * Nh), Nv, Ni, 1, Nh)),
+        )
+
+        array1 = ArrayType{FT}(undef, nl, ncol)
+        array1t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array1, data1, Val(false))
+        data2array_rrtmgp!(array1t, data1, Val(true))
+
+        @test array1 == ArrayType(transpose(array1t))
+
+        array2 = ArrayType(reshape(FT(1.0):(Nv * Ni * Nh), Nv, :))
+        array2t = ArrayType{FT}(undef, ncol, nl)
+
+        data2 = VIFH{FT, Nv, Ni}(ArrayType{FT}(undef, Nv, Ni, 1, Nh))
+
+        array2data_rrtmgp!(data2, array2, Val(false))
+        @test parent(data2) == parent(data1)
+
+        array2t = ArrayType(transpose(Array(array2)))
+
+        parent(data2) .= NaN
+        array2data_rrtmgp!(data2, array2t, Val(true))
+        @test parent(data2) == parent(data1)
+
+        data3 = VIFH{WVector{FT}, Nv, Ni}(
+            ArrayType(reshape(FT(1.0):(Nv * Ni * Nh), Nv, Ni, 1, Nh)),
+        )
+        array3 = ArrayType{FT}(undef, nl, ncol)
+        array3t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array3, data3, Val(false))
+        data2array_rrtmgp!(array3t, data3, Val(true))
+
+        @test array3 == ArrayType(transpose(array3t))
+
+
+        array4 = ArrayType(reshape(FT(1.0):(Nv * Ni * Nh), Nv, :))
+        array4t = ArrayType{FT}(undef, ncol, nl)
+
+        data4 = VIFH{FT, Nv, Ni}(ArrayType{FT}(undef, Nv, Ni, 1, Nh))
+
+        array2data_rrtmgp!(data4, array4, Val(false))
+        @test parent(data4) == parent(data4)
+    end
+end
+
+@testset "VF data2array_rrtmgp!" begin
+    Nv = 10 # number of vertical levels
+
+    nl = Nv # number of levels/layers in the array
+    ncol = 1# number of columns in the array
+
+    for FT in (Float32, Float64)
+        data1 = VF{FT, Nv}(ArrayType(reshape(FT(1.0):(Nv), Nv, 1)))
+
+        array1 = ArrayType{FT}(undef, nl, ncol)
+        array1t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array1, data1, Val(false))
+        data2array_rrtmgp!(array1t, data1, Val(true))
+
+        @test array1 == ArrayType(transpose(array1t))
+
+        array2 = ArrayType(reshape(FT(1.0):(Nv), Nv, :))
+        array2t = ArrayType{FT}(undef, ncol, nl)
+
+        data2 = VF{FT, Nv}(ArrayType{FT}(undef, Nv, 1))
+
+        array2data_rrtmgp!(data2, array2, Val(false))
+        @test parent(data2) == parent(data1)
+
+        array2t = ArrayType(transpose(Array(array2)))
+
+        parent(data2) .= NaN
+        array2data_rrtmgp!(data2, array2t, Val(true))
+        @test parent(data2) == parent(data1)
+
+        data3 = VF{WVector{FT}, Nv}(ArrayType(reshape(FT(1.0):(Nv), Nv, 1)))
+        array3 = ArrayType{FT}(undef, nl, ncol)
+        array3t = ArrayType{FT}(undef, ncol, nl)
+
+        data2array_rrtmgp!(array3, data3, Val(false))
+        data2array_rrtmgp!(array3t, data3, Val(true))
+
+        @test array3 == ArrayType(transpose(array3t))
+
+
+        array4 = ArrayType(reshape(FT(1.0):(Nv), Nv, :))
+        array4t = ArrayType{FT}(undef, ncol, nl)
+
+        data4 = VF{FT, Nv}(ArrayType{FT}(undef, Nv, 1))
+
+        array2data_rrtmgp!(data4, array4, Val(false))
+        @test parent(data4) == parent(data4)
     end
 end
