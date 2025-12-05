bc2791e22cbdf6d18df7a39d9b68e3449dac9c51
Pete Bachant <pbachant@caltech.edu>
2025-12-04 18:51:14 -0800

    Revert "Try shared memory"
    This reverts commit 5993d78a7941db1e4f16887c86c636525a0f02ca.

--- ext/cuda/matrix_fields_single_field_solve.jl
+++ ext/cuda/matrix_fields_single_field_solve.jl
@@ -127,9 +127,6 @@ function _single_field_solve!(
 end
 
 using StaticArrays: MArray
-
-# Optimized tridiagonal solver that minimizes register usage
-# by only keeping current/previous values in registers
 function band_matrix_solve_local_mem!(
     t::Type{<:MatrixFields.TridiagonalMatrixRow},
     cache,
@@ -142,41 +139,28 @@ function band_matrix_solve_local_mem!(
     A₋₁, A₀, A₊₁ = Aⱼs
     vi = vindex
 
-    # Forward elimination: only keep two rows of data in registers at a time
-    # to minimize register pressure
-    @inbounds begin
-        # First row
-        inv_D₀ = inv(A₀[vi(1)])
-        U₊₁_curr = inv_D₀ ⊠ A₊₁[vi(1)]
-        Ux_curr = inv_D₀ ⊠ b[vi(1)]
-        Ux[vi(1)] = Ux_curr
-        U₊₁[vi(1)] = U₊₁_curr
-
-        # Forward elimination loop
-        for i in 2:Nv
-            A₋₁_curr = A₋₁[vi(i)]
-            inv_D₀ = inv(A₀[vi(i)] ⊟ A₋₁_curr ⊠ U₊₁_curr)
-            Ux_curr = inv_D₀ ⊠ (b[vi(i)] ⊟ A₋₁_curr ⊠ Ux_curr)
-            Ux[vi(i)] = Ux_curr
-            if i < Nv
-                U₊₁_curr = inv_D₀ ⊠ A₊₁[vi(i)]
-                U₊₁[vi(i)] = U₊₁_curr
-            end
-        end
-
-        # Back substitution loop
-        x[vi(Nv)] = Ux[vi(Nv)]
-        i = (Nv - 1)
-        while i ≥ 1
-            x[vi(i)] = Ux[vi(i)] ⊟ U₊₁[vi(i)] ⊠ x[vi(i + 1)]
-            i -= 1
-        end
+    Ux_local = MArray{Tuple{Nv}, eltype(Ux)}(undef)
+    U₊₁_local = MArray{Tuple{Nv}, eltype(U₊₁)}(undef)
+    x_local = MArray{Tuple{Nv}, eltype(x)}(undef)
+    A₋₁_local = MArray{Tuple{Nv}, eltype(A₋₁)}(undef)
+    A₀_local = MArray{Tuple{Nv}, eltype(A₀)}(undef)
+    A₊₁_local = MArray{Tuple{Nv}, eltype(A₊₁)}(undef)
+    b_local = MArray{Tuple{Nv}, eltype(b)}(undef)
+    @inbounds for v in 1:Nv
+        A₋₁_local[v] = A₋₁[vi(v)]
+        A₀_local[v] = A₀[vi(v)]
+        A₊₁_local[v] = A₊₁[vi(v)]
+        b_local[v] = b[vi(v)]
+    end
+    cache_local = (Ux_local, U₊₁_local)
+    Aⱼs_local = (A₋₁_local, A₀_local, A₊₁_local)
+    band_matrix_solve!(t, cache_local, x_local, Aⱼs_local, b_local, identity)
+    @inbounds for v in 1:Nv
+        x[vi(v)] = x_local[v]
     end
     return nothing
 end
 
-# Optimized pentadiagonal solver that minimizes register usage
-# by only keeping necessary row values in registers
 function band_matrix_solve_local_mem!(
     t::Type{<:MatrixFields.PentadiagonalMatrixRow},
     cache,
@@ -188,65 +172,29 @@ function band_matrix_solve_local_mem!(
     Nv = DataLayouts.nlevels(x)
     Ux, U₊₁, U₊₂ = cache
     A₋₂, A₋₁, A₀, A₊₁, A₊₂ = Aⱼs
-
-    @inbounds begin
-        # First row
-        inv_D₀ = inv(A₀[vi(1)])
-        Ux_i = inv_D₀ ⊠ b[vi(1)]
-        U₊₁_i = inv_D₀ ⊠ A₊₁[vi(1)]
-        U₊₂_i = inv_D₀ ⊠ A₊₂[vi(1)]
-        Ux[vi(1)] = Ux_i
-        U₊₁[vi(1)] = U₊₁_i
-        U₊₂[vi(1)] = U₊₂_i
-
-        # Second row
-        inv_D₀ = inv(A₀[vi(2)] ⊟ A₋₁[vi(2)] ⊠ U₊₁_i)
-        Ux_i_prev = Ux_i
-        U₊₁_i_prev = U₊₁_i
-        U₊₂_i_prev = U₊₂_i
-        Ux_i = inv_D₀ ⊠ (b[vi(2)] ⊟ A₋₁[vi(2)] ⊠ Ux_i_prev)
-        U₊₁_i = inv_D₀ ⊠ (A₊₁[vi(2)] ⊟ A₋₁[vi(2)] ⊠ U₊₂_i_prev)
-        U₊₂_i = inv_D₀ ⊠ A₊₂[vi(2)]
-        Ux[vi(2)] = Ux_i
-        U₊₁[vi(2)] = U₊₁_i
-        U₊₂[vi(2)] = U₊₂_i
-
-        # Main forward elimination loop: rows 3 to Nv
-        for i in 3:Nv
-            L₋₁ = A₋₁[vi(i)] ⊟ A₋₂[vi(i)] ⊠ U₊₁_i_prev
-            inv_D₀ = inv(
-                A₀[vi(i)] ⊟ L₋₁ ⊠ U₊₁_i ⊟ A₋₂[vi(i)] ⊠ U₊₂_i_prev,
-            )
-            Ux_i_prev_prev = Ux_i_prev
-            Ux_i_prev = Ux_i
-            U₊₁_i_prev = U₊₁_i
-            U₊₂_i_prev = U₊₂_i
-            Ux_i =
-                inv_D₀ ⊠
-                (b[vi(i)] ⊟ L₋₁ ⊠ Ux_i_prev ⊟ A₋₂[vi(i)] ⊠ Ux_i_prev_prev)
-            if i < Nv
-                U₊₁_i = inv_D₀ ⊠ (A₊₁[vi(i)] ⊟ L₋₁ ⊠ U₊₂_i_prev)
-            end
-            if i < Nv - 1
-                U₊₂_i = inv_D₀ ⊠ A₊₂[vi(i)]
-            end
-            Ux[vi(i)] = Ux_i
-            if i < Nv
-                U₊₁[vi(i)] = U₊₁_i
-            end
-            if i < Nv - 1
-                U₊₂[vi(i)] = U₊₂_i
-            end
-        end
-
-        # Back substitution loop
-        x[vi(Nv)] = Ux[vi(Nv)]
-        x[vi(Nv - 1)] = Ux[vi(Nv - 1)] ⊟ U₊₁[vi(Nv - 1)] ⊠ x[vi(Nv)]
-        i = (Nv - 2)
-        while i ≥ 1
-            x[vi(i)] = Ux[vi(i)] ⊟ U₊₁[vi(i)] ⊠ x[vi(i + 1)] ⊟ U₊₂[vi(i)] ⊠ x[vi(i + 2)]
-            i -= 1
-        end
+    Ux_local = MArray{Tuple{Nv}, eltype(Ux)}(undef)
+    U₊₁_local = MArray{Tuple{Nv}, eltype(U₊₁)}(undef)
+    U₊₂_local = MArray{Tuple{Nv}, eltype(U₊₂)}(undef)
+    x_local = MArray{Tuple{Nv}, eltype(x)}(undef)
+    A₋₂_local = MArray{Tuple{Nv}, eltype(A₋₂)}(undef)
+    A₋₁_local = MArray{Tuple{Nv}, eltype(A₋₁)}(undef)
+    A₀_local = MArray{Tuple{Nv}, eltype(A₀)}(undef)
+    A₊₁_local = MArray{Tuple{Nv}, eltype(A₊₁)}(undef)
+    A₊₂_local = MArray{Tuple{Nv}, eltype(A₊₂)}(undef)
+    b_local = MArray{Tuple{Nv}, eltype(b)}(undef)
+    @inbounds for v in 1:Nv
+        A₋₂_local[v] = A₋₂[vi(v)]
+        A₋₁_local[v] = A₋₁[vi(v)]
+        A₀_local[v] = A₀[vi(v)]
+        A₊₁_local[v] = A₊₁[vi(v)]
+        A₊₂_local[v] = A₊₂[vi(v)]
+        b_local[v] = b[vi(v)]
+    end
+    cache_local = (Ux_local, U₊₁_local, U₊₂_local)
+    Aⱼs_local = (A₋₂_local, A₋₁_local, A₀_local, A₊₁_local, A₊₂_local)
+    band_matrix_solve!(t, cache_local, x_local, Aⱼs_local, b_local, identity)
+    @inbounds for v in 1:Nv
+        x[vi(v)] = x_local[v]
     end
     return nothing
 end
