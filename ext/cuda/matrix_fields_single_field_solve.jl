c56d31689b1c56e197549ad324a08b7816a489b6
Pete Bachant <pbachant@caltech.edu>
2025-12-04 18:57:43 -0800

    Make banded GPU solvers adaptive

--- ext/cuda/matrix_fields_single_field_solve.jl
+++ ext/cuda/matrix_fields_single_field_solve.jl
@@ -127,14 +127,11 @@ function _single_field_solve!(
 end
 
 using StaticArrays: MArray
-function band_matrix_solve_local_mem!(
-    t::Type{<:MatrixFields.TridiagonalMatrixRow},
-    cache,
-    x,
-    Aⱼs,
-    b,
-)
-    Nv = DataLayouts.nlevels(x)
+const REGISTER_PATH_MAX_NV = 16
+
+@inline use_register_path(Nv::Integer) = Nv ≤ REGISTER_PATH_MAX_NV
+
+@inline function _band_matrix_solve_tridi_static!(Nv, cache, x, Aⱼs, b)
     Ux, U₊₁ = cache
     A₋₁, A₀, A₊₁ = Aⱼs
     vi = vindex
@@ -154,22 +151,67 @@ function band_matrix_solve_local_mem!(
     end
     cache_local = (Ux_local, U₊₁_local)
     Aⱼs_local = (A₋₁_local, A₀_local, A₊₁_local)
-    band_matrix_solve!(t, cache_local, x_local, Aⱼs_local, b_local, identity)
+    band_matrix_solve!(
+        MatrixFields.TridiagonalMatrixRow,
+        cache_local,
+        x_local,
+        Aⱼs_local,
+        b_local,
+        identity,
+    )
     @inbounds for v in 1:Nv
         x[vi(v)] = x_local[v]
     end
-    return nothing
+end
+
+@inline function _band_matrix_solve_tridi_stream!(Nv, cache, x, Aⱼs, b)
+    Ux, U₊₁ = cache
+    A₋₁, A₀, A₊₁ = Aⱼs
+    vi = vindex
+
+    @inbounds begin
+        inv_D₀ = inv(A₀[vi(1)])
+        U₊₁_curr = inv_D₀ ⊠ A₊₁[vi(1)]
+        Ux_curr = inv_D₀ ⊠ b[vi(1)]
+        Ux[vi(1)] = Ux_curr
+        U₊₁[vi(1)] = U₊₁_curr
+
+        for i in 2:Nv
+            A₋₁_curr = A₋₁[vi(i)]
+            inv_D = inv(A₀[vi(i)] ⊟ A₋₁_curr ⊠ U₊₁_curr)
+            Ux_curr = inv_D ⊠ (b[vi(i)] ⊟ A₋₁_curr ⊠ Ux_curr)
+            Ux[vi(i)] = Ux_curr
+            if i < Nv
+                U₊₁_curr = inv_D ⊠ A₊₁[vi(i)]
+                U₊₁[vi(i)] = U₊₁_curr
+            end
+        end
+
+        x[vi(Nv)] = Ux[vi(Nv)]
+        for i in (Nv - 1):-1:1
+            x[vi(i)] = Ux[vi(i)] ⊟ U₊₁[vi(i)] ⊠ x[vi(i + 1)]
+        end
+    end
 end
 
 function band_matrix_solve_local_mem!(
-    t::Type{<:MatrixFields.PentadiagonalMatrixRow},
+    t::Type{<:MatrixFields.TridiagonalMatrixRow},
     cache,
     x,
     Aⱼs,
     b,
 )
-    vi = vindex
     Nv = DataLayouts.nlevels(x)
+    if use_register_path(Nv)
+        _band_matrix_solve_tridi_static!(Nv, cache, x, Aⱼs, b)
+    else
+        _band_matrix_solve_tridi_stream!(Nv, cache, x, Aⱼs, b)
+    end
+    return nothing
+end
+
+@inline function _band_matrix_solve_penta_static!(Nv, cache, x, Aⱼs, b)
+    vi = vindex
     Ux, U₊₁, U₊₂ = cache
     A₋₂, A₋₁, A₀, A₊₁, A₊₂ = Aⱼs
     Ux_local = MArray{Tuple{Nv}, eltype(Ux)}(undef)
@@ -192,9 +234,87 @@ function band_matrix_solve_local_mem!(
     end
     cache_local = (Ux_local, U₊₁_local, U₊₂_local)
     Aⱼs_local = (A₋₂_local, A₋₁_local, A₀_local, A₊₁_local, A₊₂_local)
-    band_matrix_solve!(t, cache_local, x_local, Aⱼs_local, b_local, identity)
+    band_matrix_solve!(
+        MatrixFields.PentadiagonalMatrixRow,
+        cache_local,
+        x_local,
+        Aⱼs_local,
+        b_local,
+        identity,
+    )
     @inbounds for v in 1:Nv
         x[vi(v)] = x_local[v]
+    end
+end
+
+@inline function _band_matrix_solve_penta_stream!(Nv, cache, x, Aⱼs, b)
+    vi = vindex
+    Ux, U₊₁, U₊₂ = cache
+    A₋₂, A₋₁, A₀, A₊₁, A₊₂ = Aⱼs
+
+    @inbounds begin
+        inv_D₀ = inv(A₀[vi(1)])
+        Ux_i = inv_D₀ ⊠ b[vi(1)]
+        U₊₁_i = inv_D₀ ⊠ A₊₁[vi(1)]
+        U₊₂_i = inv_D₀ ⊠ A₊₂[vi(1)]
+        Ux[vi(1)] = Ux_i
+        U₊₁[vi(1)] = U₊₁_i
+        U₊₂[vi(1)] = U₊₂_i
+
+        inv_D₀ = inv(A₀[vi(2)] ⊟ A₋₁[vi(2)] ⊠ U₊₁_i)
+        Ux_i_prev = Ux_i
+        U₊₁_i_prev = U₊₁_i
+        U₊₂_i_prev = U₊₂_i
+        Ux_i = inv_D₀ ⊠ (b[vi(2)] ⊟ A₋₁[vi(2)] ⊠ Ux_i_prev)
+        U₊₁_i = inv_D₀ ⊠ (A₊₁[vi(2)] ⊟ A₋₁[vi(2)] ⊠ U₊₂_i_prev)
+        U₊₂_i = inv_D₀ ⊠ A₊₂[vi(2)]
+        Ux[vi(2)] = Ux_i
+        U₊₁[vi(2)] = U₊₁_i
+        U₊₂[vi(2)] = U₊₂_i
+
+        for i in 3:Nv
+            L₋₁ = A₋₁[vi(i)] ⊟ A₋₂[vi(i)] ⊠ U₊₁_i_prev
+            inv_D₀ = inv(A₀[vi(i)] ⊟ L₋₁ ⊠ U₊₁_i ⊟ A₋₂[vi(i)] ⊠ U₊₂_i_prev)
+            Ux_i_prev_prev = Ux_i_prev
+            Ux_i_prev = Ux_i
+            U₊₁_i_prev = U₊₁_i
+            U₊₂_i_prev = U₊₂_i
+            Ux_i = inv_D₀ ⊠ (b[vi(i)] ⊟ L₋₁ ⊠ Ux_i_prev ⊟ A₋₂[vi(i)] ⊠ Ux_i_prev_prev)
+            if i < Nv
+                U₊₁_i = inv_D₀ ⊠ (A₊₁[vi(i)] ⊟ L₋₁ ⊠ U₊₂_i_prev)
+            end
+            if i < Nv - 1
+                U₊₂_i = inv_D₀ ⊠ A₊₂[vi(i)]
+            end
+            Ux[vi(i)] = Ux_i
+            if i < Nv
+                U₊₁[vi(i)] = U₊₁_i
+            end
+            if i < Nv - 1
+                U₊₂[vi(i)] = U₊₂_i
+            end
+        end
+
+        x[vi(Nv)] = Ux[vi(Nv)]
+        x[vi(Nv - 1)] = Ux[vi(Nv - 1)] ⊟ U₊₁[vi(Nv - 1)] ⊠ x[vi(Nv)]
+        for i in (Nv - 2):-1:1
+            x[vi(i)] = Ux[vi(i)] ⊟ U₊₁[vi(i)] ⊠ x[vi(i + 1)] ⊟ U₊₂[vi(i)] ⊠ x[vi(i + 2)]
+        end
+    end
+end
+
+function band_matrix_solve_local_mem!(
+    t::Type{<:MatrixFields.PentadiagonalMatrixRow},
+    cache,
+    x,
+    Aⱼs,
+    b,
+)
+    Nv = DataLayouts.nlevels(x)
+    if use_register_path(Nv)
+        _band_matrix_solve_penta_static!(Nv, cache, x, Aⱼs, b)
+    else
+        _band_matrix_solve_penta_stream!(Nv, cache, x, Aⱼs, b)
     end
     return nothing
 end
