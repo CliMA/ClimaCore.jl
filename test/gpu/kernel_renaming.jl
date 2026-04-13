5157341afe5f20dd57e5192c31c080afd5fd96ca
imreddyTeja <treddy0205@gmail.com>
2026-04-13 12:32:50 -0700

    Add gpu latency regression test
    Add logging

--- test/gpu/kernel_renaming.jl
+++ test/gpu/kernel_renaming.jl
@@ -8,6 +8,7 @@ ClimaComms.@import_required_backends
 @testset "kernel renaming" begin
     ext = Base.get_extension(ClimaCore, :ClimaCoreCUDAExt)
     @assert !isnothing(ext) # cuda must be loaded to test this extension
+    @assert ext.NAME_KERNELS_FROM_STACK_TRACE[]
     space = ExtrudedCubedSphereSpace(Float32;
         z_elem = 10,
         z_min = 0,
