b274586fb126e02118b4d9e53a6d72b07e1503d8
Kevin Phan <98072684+ph-kev@users.noreply.github.com>
2026-08-28 10:08:53 -0700

    Remove ClimaCore dependency from ClimaCoreSpectra
    This commit also cleans up the Project.toml files.

--- lib/ClimaCoreSpectra/src/ClimaCoreSpectra.jl
+++ lib/ClimaCoreSpectra/src/ClimaCoreSpectra.jl
@@ -2,8 +2,6 @@ module ClimaCoreSpectra
 
 export compute_gaussian!, power_spectrum_1d, power_spectrum_2d
 
-import ClimaCore: Geometry, Meshes, Domains, Topologies, Spaces, Fields
-
 include("gcm_spectra.jl")
 
 end # module
