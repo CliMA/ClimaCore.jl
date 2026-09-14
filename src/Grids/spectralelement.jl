a58c240bcc8a4b6918f12a505649b0707e3cb300
imreddyTeja <treddy0205@gmail.com>
2026-09-14 15:09:46 +0530

    Delete device side grids
    Instead use the same structs as the host,
    but with some fields adapted into `nothing`.
    This also requires changes all the grids
    into non-mutable structs.
    
    They were originally made mutable for faster compilation.
    I noticed a negligable slowdown in compilation (~1%)

--- src/Grids/spectralelement.jl
+++ src/Grids/spectralelement.jl
@@ -51,7 +51,7 @@ discontinuous ([`DG`](@ref)) Galerkin, and follows the quadrature when omitted;
 see [`SpectralElementGrid2D`](@ref). Construction is memoized in
 `Cache.OBJECT_CACHE`.
 """
-mutable struct SpectralElementGrid1D{
+struct SpectralElementGrid1D{
     T,
     Q,
     GG <: Geometry.AbstractGlobalGeometry,
@@ -176,7 +176,7 @@ end
 Two-dimensional spectral element grid: within each element the space is represented
 as a polynomial. See the constructor for the keyword options.
 """
-mutable struct SpectralElementGrid2D{
+struct SpectralElementGrid2D{
     T,
     Q,
     GG <: Geometry.AbstractGlobalGeometry,
@@ -769,25 +769,6 @@ global_geometry(grid::AbstractSpectralElementGrid) = grid.global_geometry
 
 quadrature_style(grid::AbstractSpectralElementGrid) = grid.quadrature_style
 dss_weights(grid::AbstractSpectralElementGrid, ::Nothing) = grid.dss_weights
-
-## GPU compatibility
-struct DeviceSpectralElementGrid1D{Q, GG, LG} <: AbstractSpectralElementGrid
-    quadrature_style::Q
-    global_geometry::GG
-    local_geometry::LG
-end
-struct DeviceSpectralElementGrid2D{Q, GG, LG, M} <: AbstractSpectralElementGrid
-    quadrature_style::Q
-    global_geometry::GG
-    local_geometry::LG
-    mask::M
-end
-
-ClimaComms.context(grid::DeviceSpectralElementGrid1D) = DeviceSideContext()
-ClimaComms.device(grid::DeviceSpectralElementGrid1D) = DeviceSideDevice()
-
-ClimaComms.context(grid::DeviceSpectralElementGrid2D) = DeviceSideContext()
-ClimaComms.device(grid::DeviceSpectralElementGrid2D) = DeviceSideDevice()
 
 ## aliases
 const RectilinearSpectralElementGrid2D =
